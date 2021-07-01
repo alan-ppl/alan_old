2bd8daaa0139b5c8571f0b900837def2662b22b1
xidulu <xidulu@gmail.com>
2021-07-01 01:09:37 -0400

    add more tests

--- test_infra.py
+++ test_infra.py
@@ -13,6 +13,7 @@ def test_special_functions():
     b = CartesianTensor(torch.ones(6, 7, 3)).refine_names('Kb','Kc', ...)
     out = torch.dot(a, b)
     assert out.shape == (5, 6, 7)
+    assert out.names == ('Ka', 'Kb', 'Kc')
     assert type(out) == type(a)
 
     # Test outer product
@@ -20,6 +21,7 @@ def test_special_functions():
     b = CartesianTensor(torch.ones(6, 7, 4)).refine_names('Kb','Kc', ...)
     out = torch.outer(a, b)
     assert out.shape == (5, 6, 7, 3, 4)
+    assert out.names == ('Ka', 'Kb', 'Kc', None, None)
     assert type(out) == type(a)
 
     print('Special function test passed...')
@@ -27,18 +29,21 @@ def test_special_functions():
 
 def test_operators():
     operators = ['+', '-', '*', '/', '**']
+    # Base cases
     a = CartesianTensor(torch.ones(6, 3)).refine_names('Ka', ...)
     b = CartesianTensor(torch.ones(6, 3)).refine_names('Kb', ...)
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (6, 6, 3)
+        assert out.names == ('Ka', 'Kb', None)
         assert type(out) == type(a)
-
+    # Share one common name
     a = CartesianTensor(torch.ones(5, 6, 3)).refine_names('Ka','Kb', ...)
     b = CartesianTensor(torch.ones(6, 7, 3)).refine_names('Kb','Kc', ...)
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (5, 6, 7, 3)
+        assert out.names == ('Ka', 'Kb', 'Kc', None)
         assert type(out) == type(a)
 
     a = CartesianTensor(torch.ones(5, 6, 3)).refine_names('Ka','Kb', ...)
@@ -46,16 +51,29 @@ def test_operators():
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (5, 6, 7, 8, 3)
+        assert out.names == ('Ka', 'Kb', 'Kc', 'Kd', None)
         assert type(out) == type(a)
 
+    # Share multiple common names
+    a = CartesianTensor(torch.ones(5, 6, 8, 3)).refine_names('Ka','Kb','Kd', ...)
+    b = CartesianTensor(torch.ones(6, 7, 8, 3)).refine_names('Kb','Kc','Kd',...)
+    for op in operators:
+        out = eval(f'a {op} b')
+        assert out.shape == (5, 6, 7, 8, 3)
+        assert out.names == ('Ka', 'Kb', 'Kc', 'Kd', None)
+        assert type(out) == type(a)
+
+    # Mixed CartesianTensor and normal tensor
     a = CartesianTensor(torch.ones(5, 6, 3)).refine_names('Ka','Kb', ...)
     b = torch.randn(3)
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (5, 6, 3)
+        assert out.names == ('Ka', 'Kb', None)
         assert type(out) == type(a)
         out = eval(f'b {op} a')
         assert out.shape == (5, 6, 3)
+        assert out.names == ('Ka', 'Kb', None)
         assert type(out) == type(a)
 
     a = CartesianTensor(torch.ones(5, 6)).refine_names('Ka','Kb')
@@ -63,8 +81,10 @@ def test_operators():
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (5, 6, 3)
+        assert out.names == ('Ka', 'Kb', None)
         out = eval(f'b {op} a')
         assert out.shape == (5, 6, 3)
+        assert out.names == ('Ka', 'Kb', None)
         assert type(out) == type(a)
 
     a = CartesianTensor(torch.ones(5, 6)).refine_names('Ka','Kb')
@@ -72,11 +92,26 @@ def test_operators():
     for op in operators:
         out = eval(f'a {op} b')
         assert out.shape == (5, 6, 3, 2, 3)
+        assert out.names == ('Ka', 'Kb', None, None, None)
         out = eval(f'b {op} a')
         assert out.shape == (5, 6, 3, 2, 3)
+        assert out.names == ('Ka', 'Kb', None, None, None)
         assert type(out) == type(a)
     print("Operator test passed...")
 
 
+def test_chain_operations():
+    k_list = ['Ka', 'Kb', 'Kc', 'Kd', 'Ke', 'Kf']
+    base_udf_shape = (4,)
+    k_shape = (5,)
+    base_tensor = torch.randn(4,) # Base udf tensor
+    for k in k_list:
+        base_tensor = base_tensor + CartesianTensor(torch.ones(k_shape)).refine_names(k,)
+    assert base_tensor.shape == len(k_list) * k_shape + base_udf_shape
+    assert base_tensor.names == tuple(k_list) + (None,)
+    print("Chain operations test passed...")
+
+
 test_operators()
 test_special_functions()
+test_chain_operations()
