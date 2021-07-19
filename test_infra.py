f60604efc08b4f27915fba6a31be2a6c2cded68b
admin <xidulu@gmail.com>
2021-07-19 12:40:27 +0800

    add a new test case

--- test_infra.py
+++ test_infra.py
@@ -86,6 +86,14 @@ def test_operators():
         assert out.names == ('Ka', 'Kb', None, None, None)
         assert type(out) == type(a)
 
+    a = CartesianTensor(torch.ones(6, 1, 1, 3).refine_names('Ka', ...))
+    b = CartesianTensor(torch.ones(6, 3).refine_names('Kb', ...))
+    for op in operators:
+        out = eval(f'a {op} b')
+        assert out.shape == (6, 6, 1, 1, 3)
+        assert out.names == ('Ka', 'Kb', None, None, None)
+        assert type(out) == type(a)
+
     # Share one common name
     a = CartesianTensor(torch.ones(5, 6, 3).refine_names('Ka', 'Kb', ...))
     b = CartesianTensor(torch.ones(6, 7, 3).refine_names('Kb', 'Kc', ...))
