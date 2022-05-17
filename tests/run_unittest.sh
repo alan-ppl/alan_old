6be9064bb7204afce235f6e20924c9dc98cc5787
ThomasHeap <thomaseheap@gmail.com>
2022-05-17 19:03:55 +0100

    Updating test script so all tests run

--- tests/run_unittest.sh
+++ tests/run_unittest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# python test_infra.py -v
-# python test_infra_cuda.py -v
-# python test_wrapped_dist.py -v
+python test_infra.py -v
+python test_infra_cuda.py -v
+python test_wrapped_dist.py -v
 python test_inference.py -v
