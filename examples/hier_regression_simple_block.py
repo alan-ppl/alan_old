e3a12afafa853c337024354f366947e7c01f216c
ThomasHeap <thomaseheap@gmail.com>
2022-09-08 20:33:28 +0100

    Updating examples

--- examples/hier_regression_simple_block.py
+++ examples/hier_regression_simple_block.py
@@ -107,7 +107,7 @@ if N == 10:
 model = tpp.Model(P, Q(), data_y)
 model.to(device)
 
-opt = t.optim.Adam(model.parameters(), lr=1E-3, betas=(0.5,0.5))
+opt = t.optim.Adam(model.parameters(), lr=1E-3)
 scheduler = t.optim.lr_scheduler.StepLR(opt, step_size=50000, gamma=0.1)
 K=args.K
 dim = tpp.make_dims(P, K, [plate_1])
@@ -119,6 +119,7 @@ for i in range(iters):
     opt.zero_grad()
     elbo = model.elbo(dims=dim)
     (-elbo).backward()
+    opt.step()
     scheduler.step()
     elbos.append(elbo.item())
     if 0 == i%1000:
