bf1f7eec09f092acd1dab8ab371883b07b86b8c9
ThomasHeap <thomaseheap@gmail.com>
2022-10-16 10:41:59 +0100

    changes to scripts

--- examples/movielens/movielens_tpp.py
+++ examples/movielens/movielens_tpp.py
@@ -116,6 +116,6 @@ for K in Ks:
         elbos.append(phi_loss.item())
     results_dict[N][M][K] = {'lower_bound':np.mean(elbos),'std':np.std(elbos), 'elbos': elbos}
 
-file = 'results/movielens_results_discrete_rws_N{0}_M{1}.json'.format(N,M)
+file = 'results/movielens_results_tpp_N{0}_M{1}.json'.format(N,M)
 with open(file, 'w') as f:
     json.dump(results_dict, f)
