89ed4dbe9ece1ac0a1c5dff539fc7d9088c1e5bb
ThomasHeap <thomaseheap@gmail.com>
2022-10-23 13:08:08 +0100

    updates to learning rate scheduling

--- examples/Experiments_2203_04432/hier_regression_IW.py
+++ examples/Experiments_2203_04432/hier_regression_IW.py
@@ -116,11 +116,10 @@ for K in Ks:
 
             if 0 == i%1000:
                 print("Iteration: {0}, ELBO: {1:.2f}".format(i,elbo.item()))
-                lr.append(opt.param_groups[0]['lr'])
-        lrs.append(lr)
+
         elbos.append(elbo.item())
 
-    results_dict[N][M][K] = {'lower_bound':np.mean(elbos),'std':np.std(elbos), 'elbos': elbos, 'lrs':lrs}
+    results_dict[N][M][K] = {'lower_bound':np.mean(elbos),'std':np.std(elbos), 'elbos': elbos}
 
 file = 'results/results_local_IW_N{0}_M{1}.json'.format(N,M)
 with open(file, 'w') as f:
--- examples/Experiments_2203_04432/hier_regression_lr.py
+++ examples/Experiments_2203_04432/hier_regression_lr.py
@@ -99,7 +99,7 @@ for K in Ks:
         model.to(device)
 
         opt = t.optim.Adam(model.parameters(), lr=1E-3)
-        scheduler = t.optim.lr_scheduler.StepLR(opt, step_size=25000, gamma=0.1)
+        scheduler = t.optim.lr_scheduler.StepLR(opt, step_size=10000, gamma=0.1)
         dim = tpp.make_dims(P, K, [plate_1])
 
         for i in range(50000):
@@ -110,11 +110,10 @@ for K in Ks:
 
             if 0 == i%1000:
                 print("Iteration: {0}, ELBO: {1:.2f}".format(i,elbo.item()))
-                lr.append(opt.param_groups[0]['lr'])
-        lrs.append(lr)
+
         elbos.append(elbo.item())
 
-    results_dict[N][M][K] = {'lower_bound':np.mean(elbos),'std':np.std(elbos), 'elbos': elbos, 'lrs':lrs}
+    results_dict[N][M][K] = {'lower_bound':np.mean(elbos),'std':np.std(elbos), 'elbos': elbos}
 
 file = 'results/results_lr_N{0}_M{1}.json'.format(N,M)
 with open(file, 'w') as f:
