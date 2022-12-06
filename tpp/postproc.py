ad3d4a06cbc48c39dab6049d8f8f6996ff0bae07
Laurence Aitchison <laurence.aitchison@gmail.com>
2022-12-06 10:11:39 +0000

    Nice postprocessing library

--- tpp/postproc.py
+++ tpp/postproc.py
@@ -9,29 +9,45 @@ def Kname(x):
     assert 1==len(Ks)
     return Ks[0]
 
-def map_sample_dict(f):
-    def inner(d):
-        return {var_name: f(sample, w) for (var_name, (sample, w)) in d.items()}
+def map_or_apply(f):
+    def inner(arg0, arg1=None):
+        if isinstance(arg0, dict):
+            assert arg1 is None
+            return {var_name: f(sample, w) for (var_name, (sample, w)) in arg0.items()}
+        else:
+            assert arg1 is not None
+            return f(arg0, arg1)
     return inner
 
-def mean_inner(sample, w):
-    return (w*sample).sum(Kname(sample))
+def Ef(f):
+    return map_or_apply(lambda sample, w: (w*f(sample)).sum(Kname(sample)))
+mean_raw     = lambda x: x
+mean2_raw    = lambda x: x**2
+p_lower_raw  = lambda value: lambda x: (x < value).to(dtype=x.dtype)
+p_higher_raw = lambda value: lambda x: (x > value).to(dtype=x.dtype)
 
-def var_inner(sample, w):
-    K = Kname(sample)
-    mean  = (w*sample   ).sum(K)
-    mean2 = (w*sample**2).sum(K)
-    return mean2 - mean**2
-
-def std_inner(sample, w):
-    return var_inner(sample, w).sqrt()
-
-def ess_inner(_, w):
-    return 1/((w**2).sum(Kname(w)))
-
-mean = map_sample_dict(mean_inner)
-var  = map_sample_dict(var_inner)
-std  = map_sample_dict(std_inner)
-ess  = map_sample_dict(ess_inner)
+mean     = Ef(mean_raw)
+mean2    = Ef(mean2_raw)
+p_lower  = lambda value: Ef(p_lower_raw(value))
+p_higher = lambda value: Ef(p_lower_raw(value))
 
 
+
+"""
+Relevant quantites that aren't plain moments
+"""
+var = map_or_apply(lambda sample, w: mean2(sample, w) - (mean(sample, w))**2)
+std = map_or_apply(lambda sample, w: var(sample, w).sqrt())
+ess = map_or_apply(lambda _, w: 1/((w**2).sum(Kname(w))))
+
+"""
+Standard errors (which only make sense for plain moments)
+Not subtracting 1 from the ESS?
+"""
+def stderr(f):
+    return map_or_apply(lambda sample, w: (var(f(sample), w) / ess(sample, w)).sqrt())
+
+stderr_mean     = stderr(mean_raw)
+stderr_mean2    = stderr(mean2_raw)
+stderr_p_lower  = lambda value: stderr(p_lower_raw(value))
+stderr_p_higher = lambda value: stderr(p_higher_raw(value))
