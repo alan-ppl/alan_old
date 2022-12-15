fixing examples, fixed numerical instability in log sum exp, fixed problem where summing singleton dimension tensors ran out of memory

examples/Deeper_Hier_Regression/Deep_Hier_Regression.py
@@ -1,9 +1,7 @@
 import torch as t
 import torch.nn as nn
 import tpp
-from tpp.prob_prog import Trace, TraceLogP, TraceSampleLogQ
-import tqdm
-from functorch.dim import dims
+
 import argparse
 import json
 import numpy as np
@@ -42,46 +40,47 @@ Ks = [1,5,10,15]
 M = args.M
 N = args.N
 
-plate_muz2, plate_muz3, plate_muz4, plate_z, plate_obs = dims(5 , [2,2,2,M,N])
+sizes = {'plate_muz2':2, 'plate_muz3':2, 'plate_muz4':2, 'plate_z':M, 'plate_obs':N}
 if N == 30:
     d_z = 20
 else:
     d_z = 5
-x = t.load('weights_{0}_{1}.pt'.format(N,M))[plate_muz2, plate_muz3, plate_muz4, plate_z, plate_obs].to(device)
+x = t.load('weights_{0}_{1}.pt'.format(N,M)).rename('plate_muz2', 'plate_muz3', 'plate_muz4', 'plate_z', 'plate_obs', ...).to(device)
 
 def P(tr):
   '''
   Heirarchical Model
   '''
 
-  tr['mu_z1'] = tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device))
-  tr['mu_z2'] = tpp.Normal(tr['mu_z1'], t.ones(()).to(device), sample_dim=plate_muz2)
-  tr['mu_z3'] = tpp.Normal(tr['mu_z2'], t.ones(()).to(device), sample_dim=plate_muz3)
-  tr['mu_z4'] = tpp.Normal(tr['mu_z3'], t.ones(()).to(device), sample_dim=plate_muz4)
-  tr['psi_z'] = tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device))
-  tr['psi_y'] = tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device))
+  tr.sample('mu_z1', tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device)))
+  tr.sample('mu_z2', tpp.Normal(tr['mu_z1'], t.ones(()).to(device)), plate='plate_muz2')
+  tr.sample('mu_z3', tpp.Normal(tr['mu_z2'], t.ones(()).to(device)), plate='plate_muz3')
+  tr.sample('mu_z4', tpp.Normal(tr['mu_z3'], t.ones(()).to(device)), plate='plate_muz4')
+  tr.sample('psi_y', tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device)))
+  tr.sample('psi_z', tpp.Normal(t.zeros(()).to(device), t.ones(()).to(device)))
 
-  tr['z'] = tpp.Normal(tr['mu_z4'] * t.ones((d_z)).to(device), tr['psi_z'].exp(), sample_dim=plate_z)
+  tr.sample('z', tpp.Normal(tr['mu_z4'] * t.ones((d_z)).to(device), tr['psi_z'].exp()), plate='plate_z')
+  print(x)
+  print(tr['z'])
+  tr.sample('obs', tpp.Normal((tr['z'] @ x), tr['psi_y'].exp()))
 
-  tr['obs'] = tpp.Normal((tr['z'] @ x), tr['psi_y'].exp())
 
 
-
-class Q(tpp.Q_module):
+class Q(tpp.Q):
     def __init__(self):
         super().__init__()
         #mu_z1
         self.reg_param("m_mu_z1", t.zeros(()))
         self.reg_param("log_theta_mu_z1", t.zeros(()))
         #mu_z2
-        self.reg_param("m_mu_z2", t.zeros((2,)), [plate_muz2])
-        self.reg_param("log_theta_mu_z2", t.zeros((2,)), [plate_muz2])
+        self.reg_param("m_mu_z2", t.zeros((2,)), ['plate_muz2'])
+        self.reg_param("log_theta_mu_z2", t.zeros((2,)), ['plate_muz2'])
         #mu_z3
-        self.reg_param("m_mu_z3", t.zeros((2,2)), [plate_muz2, plate_muz3])
-        self.reg_param("log_theta_mu_z3", t.zeros((2,2)), [plate_muz2, plate_muz3])
+        self.reg_param("m_mu_z3", t.zeros((2,2)), ['plate_muz2', 'plate_muz3'])
+        self.reg_param("log_theta_mu_z3", t.zeros((2,2)), ['plate_muz2', 'plate_muz3'])
         #mu_z4
-        self.reg_param("m_mu_z4", t.zeros((2,2,2)), [plate_muz2, plate_muz3, plate_muz4])
-        self.reg_param("log_theta_mu_z4", t.zeros((2,2,2)), [plate_muz2, plate_muz3, plate_muz4])
+        self.reg_param("m_mu_z4", t.zeros((2,2,2)), ['plate_muz2', 'plate_muz3', 'plate_muz4'])
+        self.reg_param("log_theta_mu_z4", t.zeros((2,2,2)), ['plate_muz2', 'plate_muz3', 'plate_muz4'])
         #psi_z
         self.reg_param("m_psi_z", t.zeros(()))
         self.reg_param("log_theta_psi_z", t.zeros(()))
@@ -90,23 +89,23 @@ class Q(tpp.Q_module):
         self.reg_param("log_theta_psi_y", t.zeros(()))
 
         #z
-        self.reg_param("mu", t.zeros((2, 2, 2, M,d_z)), [plate_muz2, plate_muz3, plate_muz4, plate_z])
-        self.reg_param("log_sigma", t.zeros((2, 2, 2, M, d_z)), [plate_muz2, plate_muz3, plate_muz4, plate_z])
+        self.reg_param("mu", t.zeros((2, 2, 2, M,d_z)), ['plate_muz2', 'plate_muz3', 'plate_muz4', 'plate_z'])
+        self.reg_param("log_sigma", t.zeros((2, 2, 2, M, d_z)), ['plate_muz2', 'plate_muz3', 'plate_muz4', 'plate_z'])
 
 
     def forward(self, tr):
-        tr['mu_z1'] = tpp.Normal(self.m_mu_z1, self.log_theta_mu_z1.exp())
-        tr['mu_z2'] = tpp.Normal(self.m_mu_z2, self.log_theta_mu_z2.exp())
-        tr['mu_z3'] = tpp.Normal(self.m_mu_z3, self.log_theta_mu_z3.exp())
-        tr['mu_z4'] = tpp.Normal(self.m_mu_z4, self.log_theta_mu_z4.exp())
-        tr['psi_z'] = tpp.Normal(self.m_psi_z, self.log_theta_psi_z.exp())
-        tr['psi_y'] = tpp.Normal(self.m_psi_y, self.log_theta_psi_y.exp())
+        tr.sample('mu_z1', tpp.Normal(self.m_mu_z1, self.log_theta_mu_z1.exp()))
+        tr.sample('mu_z2', tpp.Normal(self.m_mu_z2, self.log_theta_mu_z2.exp()))
+        tr.sample('mu_z3', tpp.Normal(self.m_mu_z3, self.log_theta_mu_z3.exp()))
+        tr.sample('mu_z4', tpp.Normal(self.m_mu_z4, self.log_theta_mu_z4.exp()))
+        tr.sample('psi_z', tpp.Normal(self.m_psi_z, self.log_theta_psi_z.exp()))
+        tr.sample('psi_y', tpp.Normal(self.m_psi_y, self.log_theta_psi_y.exp()))
 
 
-        tr['z'] = tpp.Normal(self.mu, self.log_sigma.exp())
+        tr.sample('z', tpp.Normal(self.mu, self.log_sigma.exp()))
 
-data_y = {'obs':t.load('data_y_{0}_{1}.pt'.format(N, M))[plate_muz2, plate_muz3, plate_muz4, plate_obs, plate_z].to(device)}
-test_data_y = {'obs':t.load('test_data_y_{0}_{1}.pt'.format(N, M))[plate_muz2, plate_muz3, plate_muz4, plate_obs, plate_z].to(device)}
+data_y = {'obs':t.load('data_y_{0}_{1}.pt'.format(N, M)).rename('plate_muz2', 'plate_muz3', 'plate_muz4', 'plate_z', 'plate_obs').to(device)}
+test_data_y = {'obs':t.load('test_data_y_{0}_{1}.pt'.format(N, M)).rename('plate_muz2', 'plate_muz3', 'plate_muz4', 'plate_z', 'plate_obs').to(device)}
 for K in Ks:
     print(K,M,N)
     results_dict[N] = results_dict.get(N, {})

examples/cartesian_model.py
@@ -1,51 +1,51 @@
 import torch as t
 import torch.nn as nn
 import tpp
-from tpp.prob_prog import Trace, TraceLogP, TraceSampleLogQ
-from tpp.backend import vi
-import tqdm
-from functorch.dim import dims
+t.manual_seed(0)
 
 def P(tr):
     scale = 0.1
-    tr['a'] = tpp.Normal(t.zeros(3,), 1)
-    tr['b'] = tpp.Normal(tr['a'] + 1, 1)
-    tr['c'] = tpp.Normal(t.zeros(3,), 1)
-    tr['d'] = tpp.Normal(tr['c'] + tr['b'], 1)
-    tr['obs'] = tpp.Normal(tr['d'], 0.1)
+    tr.sample('a', tpp.Normal(t.zeros(3,), 1))
+    tr.sample('b', tpp.Normal(tr['a'] + 1, 1))
+    tr.sample('c', tpp.Normal(t.zeros(3,), 1))
+    tr.sample('d', tpp.Normal(tr['c'] + tr['b'], 1))
+    tr.sample('obs', tpp.Normal(tr['d'], 0.1))
 
 
-class Q(nn.Module):
+class Q(tpp.Q):
     def __init__(self):
         super().__init__()
-        self.m_a = nn.Parameter(t.zeros(3,))
-        self.m_b = nn.Parameter(t.zeros(3,))
-        self.m_c = nn.Parameter(t.zeros(3,))
-        self.m_d = nn.Parameter(t.zeros(3,))
+        self.reg_param('m_a', t.zeros(3,))
+        self.reg_param('m_b', t.zeros(3,))
+        self.reg_param('m_c', t.zeros(3,))
+        self.reg_param('m_d', t.zeros(3,))
 
-        self.log_s_a = nn.Parameter(t.zeros(3,))
-        self.log_s_b = nn.Parameter(t.zeros(3,))
-        self.log_s_c = nn.Parameter(t.zeros(3,))
-        self.log_s_d = nn.Parameter(t.zeros(3,))
+        self.reg_param('log_s_a', t.zeros(3,))
+        self.reg_param('log_s_b', t.zeros(3,))
+        self.reg_param('log_s_c', t.zeros(3,))
+        self.reg_param('log_s_d', t.zeros(3,))
+
 
     def forward(self, tr):
-        tr['a'] = tpp.Normal(self.m_a, self.log_s_a.exp())
-        tr['b'] = tpp.Normal(self.m_b, self.log_s_b.exp())
-        tr['c'] = tpp.Normal(self.m_c, self.log_s_c.exp())
-        tr['d'] = tpp.Normal(self.m_d, self.log_s_d.exp())
+        tr.sample('a', tpp.Normal(self.m_a, self.log_s_a.exp()))
+        tr.sample('b', tpp.Normal(self.m_b, self.log_s_b.exp()))
+        tr.sample('c', tpp.Normal(self.m_c, self.log_s_c.exp()))
+        tr.sample('d', tpp.Normal(self.m_d, self.log_s_d.exp()))
 
-data = tpp.sample(P, "obs")
+
+
+data = tpp.sample(P,varnames=('obs',))
 
 model = tpp.Model(P, Q(), data)
 
 opt = t.optim.Adam(model.parameters(), lr=1E-2)
 
 K=10
-dim = tpp.make_dims(P, K)
+
 print("K={}".format(K))
 for i in range(1000):
     opt.zero_grad()
-    elbo = model.elbo(dims=dim)
+    elbo = model.elbo(K=K)
     (-elbo).backward()
     opt.step()
 

tpp/model.py
@@ -6,7 +6,7 @@ from .utils import *
 class Q(nn.Module):
     """
     Key problem: parameters in Q must have torchdim plates.
-    Solve this problem by making a new method to register parameters, "reg_param", which takes 
+    Solve this problem by making a new method to register parameters, "reg_param", which takes
     a named tensor, and builds up a mapping from names to torchdims.
     """
     def __init__(self):
@@ -47,8 +47,8 @@ class Q(nn.Module):
 class Model(nn.Module):
     """
     Plate dimensions come from data.
-    Model(P, Q, data) is for non-minibatched data. 
-    elbo(K=10, data) is for minibatched data. 
+    Model(P, Q, data) is for non-minibatched data.
+    elbo(K=10, data) is for minibatched data.
 
     data is stored as torchdim
     """
@@ -57,7 +57,7 @@ class Model(nn.Module):
         self.P = P
         self.Q = Q
 
-        
+
         if data is None:
             data = {}
         self.data, self.plates = named2dim_data(data, Q._plates)
@@ -66,7 +66,7 @@ class Model(nn.Module):
         data, plates = named2dim_data(data, self.plates)
         all_data = {**self.data, **data}
         assert len(all_data) == len(self.data) + len(data)
-       
+
         #sample from approximate posterior
         trq = TraceQ(K, all_data, plates, reparam)
         self.Q(trq)
@@ -107,7 +107,7 @@ class Model(nn.Module):
 #        lls_train = trace_pred.ll_train
 #        assert set(ll_all.keys()) == set(ll_train.keys())
 #        total = 0.
-#        for varname in ll_all:        
+#        for varname in ll_all:
 #            ll_all   = lls_all[varname]
 #            ll_train = lls_train[varname]
 #