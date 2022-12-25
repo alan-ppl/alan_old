602a335e3a535d3816288a2c82c9cbf4701a2ab9
Laurence Aitchison <laurence.aitchison@gmail.com>
2022-12-25 10:13:41 +0000

    Errors and tests for ML

---
+++ tests/test_ml.py
@@ -0,0 +1,10 @@
+from tpp.ml_updates import *
+
+if __name__ == "__main__":
+    N = 10
+    MLBernoulli.test(N)
+    MLPoisson.test(N)
+    MLNormal.test(N)
+    MLExponential.test(N)
+    MLDirichlet.test(N)
+    MLGamma.test(N)
