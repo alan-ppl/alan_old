2c35fe42fd9fd477192384e9b4f457588a6b9f3f
ThomasHeap <thomaseheap@gmail.com>
2023-02-05 11:00:12 +0000

    updating bus breakdown script

--- examples/experiments/run_bus.sh
+++ examples/experiments/run_bus.sh
@@ -1,4 +1,4 @@
-lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_tmc training.pred_ll.do_pred_ll=True training.num_iters=75000
-lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_tmc_new training.pred_ll.do_pred_ll=True training.num_iters=75000
-lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_global training.pred_ll.do_pred_ll=True training.num_iters=75000
+lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --gputype rtx_3090 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_tmc training.pred_ll.do_pred_ll=True training.num_iters=50000
+lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --gputype rtx_3090 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_tmc_new training.pred_ll.do_pred_ll=True training.num_iters=50000
+lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --gputype rtx_3090 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_global training.pred_ll.do_pred_ll=True training.num_iters=50000
 # lbatch -c 1 -g 1 -m 22 -t 25 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.inference_method=rws_tmc_new local=True training.pred_ll.do_pred_ll=True training.num_iters=50000
