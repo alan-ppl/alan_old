d2ab095ded80fd21d83521b01db091589a497dfb
ThomasHeap <thomaseheap@gmail.com>
2023-02-08 11:42:37 +0000

    Fixing scripts

--- plotting/movielens/plot_rws.py
+++ plotting/movielens/plot_rws.py
@@ -48,9 +48,9 @@ with plt.rc_context(bundles.icml2022()):
             elbos_rws_IW = [results_IW[N][M][k]['pred_likelihood'] for k in Ks]
             stds_rws_IW = [results_IW[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
 
-            ax[i,j].errorbar(Ks,elbos_rws_global_k, yerr=stds_rws_global_k, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Globally Importance Weighted RWS')
+            ax[i,j].errorbar(Ks,elbos_rws_global_k, yerr=stds_rws_global_k, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Global RWS')
             # ax[i,j].errorbar(Ks,elbos_rws_tmc, yerr=stds_rws_tmc, linewidth=0.55, markersize = 0.75, fmt='-o', c='orange', label='TMC RWS')
-            ax[i,j].errorbar(Ks,elbos_rws_tmc_new, yerr=stds_rws_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='Massively Parallel RWS')
+            ax[i,j].errorbar(Ks,elbos_rws_tmc_new, yerr=stds_rws_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP RWS')
             # ax[i,j].errorbar(Ks,elbos_rws_IW, yerr=stds_rws_IW, linewidth=0.55, markersize = 0.75, fmt='-o', c='purple', label='RWS LIW')
 
             count =+ 1
@@ -74,64 +74,64 @@ with plt.rc_context(bundles.icml2022()):
     plt.savefig('charts/chart_movielens_rws.pdf'.format(N, M))
 
 
-plt.rcParams.update({"figure.dpi": 300})
-with plt.rc_context(bundles.icml2022()):
-
-    count = 0
-    fig, ax = plt.subplots(2,3, figsize=(5.5, 3.5))
-    for i in range(len(Ns)):
-        for j in range(len(Ms)):
-            N = Ns[i]
-            M = Ms[j]
-
-            with open('results/movielens_discrete/rws_global_N{0}_M{1}.json'.format(N,M)) as f:
-                results_rws_global_k = json.load(f)
-
-            with open('results/movielens_discrete/rws_tmc_N{0}_M{1}.json'.format(N,M)) as f:
-                results_rws_tmc = json.load(f)
-
-            with open('results/movielens_discrete/rws_tmc_new_N{0}_M{1}.json'.format(N,M)) as f:
-                results_rws_tmc_new = json.load(f)
-
-            with open('results/movielens_discrete/rws_tmc_new_LIW_N{0}_M{1}.json'.format(N,M)) as f:
-                results_IW = json.load(f)
-
-
-
-            elbos_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood'] for k in Ks]
-            stds_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
-            time_rws_tmc_new = [results_rws_tmc_new[N][M][k]['avg_time'] for k in Ks]
-            stds_rws_tmc_new_time = [results_rws_tmc_new[N][M][k]['std_time']/np.sqrt(5) for k in Ks]
-
-
-            elbos_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood'] for k in Ks]
-            stds_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
-            time_rws_global_k = [results_rws_global_k[N][M][k]['avg_time'] for k in Ks]
-            stds_rws_global_k_time = [results_rws_global_k[N][M][k]['std_time']/np.sqrt(5) for k in Ks]
-
-
-
-            ax[i,j].errorbar(time_rws_global_k,elbos_rws_global_k, xerr=stds_rws_global_k_time, yerr=stds_rws_global_k, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Globally Importance Weighted RWS')
-            # ax[i,j].errorbar(Ks,elbos_rws_tmc, yerr=stds_rws_tmc, linewidth=0.55, markersize = 0.75, fmt='-o', c='orange', label='TMC RWS')
-            ax[i,j].errorbar(time_rws_tmc_new,elbos_rws_tmc_new, xerr=stds_rws_tmc_new_time, yerr=stds_rws_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='Massively Parallel RWS')
-            # ax[i,j].errorbar(Ks,elbos_rws_IW, yerr=stds_rws_IW, linewidth=0.55, markersize = 0.75, fmt='-o', c='purple', label='RWS LIW')
-
-            count =+ 1
-    # plt.title('Groups: 0, Observations per group: 1, with one standard deviation')
-    ax[0,0].set_title('Number of users = 50')
-    ax[0,1].set_title('Number of users = 150')
-    ax[0,2].set_title('Number of users = 300')
-
-    ax[0,0].set_ylabel('Films per user = 5 \n Predictive Log Likelihood')
-    ax[1,0].set_ylabel('Films per user = 10 \n Predictive Log Likelihood')
-
-    ax[1,0].sharex(ax[0,0])
-    ax[1,0].set_xlabel('Time (Seconds)')
-    ax[1,1].sharex(ax[0,0])
-    ax[1,1].set_xlabel('Time (Seconds)')
-    ax[1,2].sharex(ax[0,0])
-    ax[1,2].set_xlabel('Time (Seconds)')
-    # fig.tight_layout()
-    plt.legend()
-    plt.savefig('charts/chart_movielens_rws_time.png'.format(N, M))
-    plt.savefig('charts/chart_movielens_rws_time.pdf'.format(N, M))
+# plt.rcParams.update({"figure.dpi": 300})
+# with plt.rc_context(bundles.icml2022()):
+#
+#     count = 0
+#     fig, ax = plt.subplots(2,3, figsize=(5.5, 3.5))
+#     for i in range(len(Ns)):
+#         for j in range(len(Ms)):
+#             N = Ns[i]
+#             M = Ms[j]
+#
+#             with open('results/movielens_discrete/rws_global_N{0}_M{1}.json'.format(N,M)) as f:
+#                 results_rws_global_k = json.load(f)
+#
+#             with open('results/movielens_discrete/rws_tmc_N{0}_M{1}.json'.format(N,M)) as f:
+#                 results_rws_tmc = json.load(f)
+#
+#             with open('results/movielens_discrete/rws_tmc_new_N{0}_M{1}.json'.format(N,M)) as f:
+#                 results_rws_tmc_new = json.load(f)
+#
+#             with open('results/movielens_discrete/rws_tmc_new_LIW_N{0}_M{1}.json'.format(N,M)) as f:
+#                 results_IW = json.load(f)
+#
+#
+#
+#             elbos_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood'] for k in Ks]
+#             stds_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
+#             time_rws_tmc_new = [results_rws_tmc_new[N][M][k]['avg_time'] for k in Ks]
+#             stds_rws_tmc_new_time = [results_rws_tmc_new[N][M][k]['std_time']/np.sqrt(5) for k in Ks]
+#
+#
+#             elbos_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood'] for k in Ks]
+#             stds_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
+#             time_rws_global_k = [results_rws_global_k[N][M][k]['avg_time'] for k in Ks]
+#             stds_rws_global_k_time = [results_rws_global_k[N][M][k]['std_time']/np.sqrt(5) for k in Ks]
+#
+#
+#
+#             ax[i,j].errorbar(time_rws_global_k,elbos_rws_global_k, xerr=stds_rws_global_k_time, yerr=stds_rws_global_k, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Globally Importance Weighted RWS')
+#             # ax[i,j].errorbar(Ks,elbos_rws_tmc, yerr=stds_rws_tmc, linewidth=0.55, markersize = 0.75, fmt='-o', c='orange', label='TMC RWS')
+#             ax[i,j].errorbar(time_rws_tmc_new,elbos_rws_tmc_new, xerr=stds_rws_tmc_new_time, yerr=stds_rws_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='Massively Parallel RWS')
+#             # ax[i,j].errorbar(Ks,elbos_rws_IW, yerr=stds_rws_IW, linewidth=0.55, markersize = 0.75, fmt='-o', c='purple', label='RWS LIW')
+#
+#             count =+ 1
+#     # plt.title('Groups: 0, Observations per group: 1, with one standard deviation')
+#     ax[0,0].set_title('Number of users = 50')
+#     ax[0,1].set_title('Number of users = 150')
+#     ax[0,2].set_title('Number of users = 300')
+#
+#     ax[0,0].set_ylabel('Films per user = 5 \n Predictive Log Likelihood')
+#     ax[1,0].set_ylabel('Films per user = 10 \n Predictive Log Likelihood')
+#
+#     ax[1,0].sharex(ax[0,0])
+#     ax[1,0].set_xlabel('Time (Seconds)')
+#     ax[1,1].sharex(ax[0,0])
+#     ax[1,1].set_xlabel('Time (Seconds)')
+#     ax[1,2].sharex(ax[0,0])
+#     ax[1,2].set_xlabel('Time (Seconds)')
+#     # fig.tight_layout()
+#     plt.legend()
+#     plt.savefig('charts/chart_movielens_rws_time.png'.format(N, M))
+#     plt.savefig('charts/chart_movielens_rws_time.pdf'.format(N, M))
--- plotting/radon/plot.py
+++ plotting/radon/plot.py
@@ -23,37 +23,33 @@ for i in range(len(Ns)):
     for j in range(len(Ms)):
         N = Ns[i]
         M = Ms[j]
-        with open('results/radon_unif_alongreadings/elbo_tmc_new_LIW_N2_M2.json') as f:
-            results_local_IW = json.load(f)
-
-        with open('results/radon_unif_alongreadings/elbo_tmc_N2_M2.json') as f:
-            results_tmc = json.load(f)
-
-        with open('results/radon_unif_alongreadings/elbo_tmc_new_N2_M2.json') as f:
-            results_tmc_new = json.load(f)
-
-        with open('results/radon_unif_alongreadings/elbo_global_N2_M2.json') as f:
-            results_global_K = json.load(f)
-
-
-        elbos_tmc = [results_tmc[N][M][k]['final_obj'] for k in Ks]
-        stds_tmc = [results_tmc[N][M][k]['final_obj_std']/np.sqrt(5) for k in Ks]
-
-        elbos_tmc_new = [results_tmc_new[N][M][k]['final_obj'] for k in Ks]
-        stds_tmc_new = [results_tmc_new[N][M][k]['final_obj_std']/np.sqrt(5) for k in Ks]
-
-        elbos_IW = [results_local_IW[N][M][k]['final_obj'] for k in Ks]
-        stds_IW = [results_local_IW[N][M][k]['final_obj_std']/np.sqrt(5) for k in Ks]
-
-        elbos_global_K = [results_global_K[N][M][k]['final_obj'] for k in Ks]
-        stds_global_K = [results_global_K[N][M][k]['final_obj_std']/np.sqrt(5) for k in Ks]
-
-
-        ax.errorbar(Ks,elbos_IW, yerr=stds_IW, linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='LIW')
-        # ax.errorbar(Ks,elbos_tmc, yerr=stds_tmc, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='TMC')
-        ax.errorbar(Ks,elbos_tmc_new, yerr=stds_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='Massively Parallel')
-        ax.errorbar(Ks,elbos_global_K, yerr=stds_global_K, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Globally Importance Weighted')
+        # with open('results/radon_discrete/rws_tmc_new_LIW_N2_M2.json') as f:
+        #     results_local_IW = json.load(f)
         #
+        # with open('results/radon_discrete/rws_tmc_N2_M2.json') as f:
+        #     results_tmc = json.load(f)
+
+        with open('results/radon_discrete/rws_tmc_new_N2_M2.json') as f:
+            results_rws_tmc_new = json.load(f)
+
+        with open('results/radon_discrete/rws_global_N2_M2.json') as f:
+            results_rws_global_k = json.load(f)
+
+
+        # elbos_rws_tmc = [results_rws_tmc[N][M][k]['pred_likelihood'] for k in Ks]
+        # stds_rws_tmc = [results_rws_tmc[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
+
+        elbos_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood'] for k in Ks]
+        stds_rws_tmc_new = [results_rws_tmc_new[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
+
+        elbos_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood'] for k in Ks]
+        stds_rws_global_k = [results_rws_global_k[N][M][k]['pred_likelihood_std']/np.sqrt(5) for k in Ks]
+
+
+        ax.errorbar(Ks,elbos_rws_global_k, yerr=stds_rws_global_k, linewidth=0.55, markersize = 0.75, fmt='-o', c='blue', label='Global RWS')
+        # ax.errorbar(Ks,elbos_rws_tmc, yerr=stds_rws_tmc, linewidth=0.55, markersize = 0.75, fmt='-o', c='orange', label='TMC RWS')
+        ax.errorbar(Ks,elbos_rws_tmc_new, yerr=stds_rws_tmc_new, linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP RWS')
+
         # ax.set_ylabel('Final Lower Bound')
         # ax.set_xlabel('K')
         # ax[i,j].label_outer()
@@ -61,7 +57,7 @@ for i in range(len(Ns)):
 # plt.title('Groups: 0, Observations per group: 1, with one standard deviation')
 # ax.set_title('4 States, 4 Counties, 4 Zipcodes, 2 Readings')
 
-ax.set_ylabel('Final Lower Bound')
+ax.set_ylabel('Predictive Log Likelihood')
 
 
 ax.set_xlabel('K')
