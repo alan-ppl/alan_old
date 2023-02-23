14fa0d7d7f5cb217b93f1749d3da2d518b45f891
ThomasHeap <thomaseheap@gmail.com>
2023-02-23 22:47:45 +0000

    update to runner script

--- examples/fast_ml_experiments/runner_ml.py
+++ examples/fast_ml_experiments/runner_ml.py
@@ -37,7 +37,7 @@ print('...', flush=True)
 @hydra.main(version_base=None, config_path='config', config_name='conf')
 def run_experiment(cfg):
     print(cfg)
-    writer = SummaryWriter(log_dir='runs/' + cfg.dataset + '/' + cfg.model + '/')
+    # writer = SummaryWriter(log_dir='runs/' + cfg.dataset + '/' + cfg.model + '/')
     device = t.device("cuda" if t.cuda.is_available() else "cpu")
 
     results_dict = {}
@@ -79,8 +79,8 @@ def run_experiment(cfg):
                 per_seed_obj.append(elbo)
                 model.update(cfg.training.lr, sample)
 
-                writer.add_scalar('Objective/Run number {}/{}'.format(i, K), elbo, j)
-                if 0 == j%1000:
+                # writer.add_scalar('Objective/Run number {}/{}'.format(i, K), elbo, j)
+                if j % 10 == 0:
                     print("Iteration: {0}, ELBO: {1:.2f}".format(j,elbo))
 
 
@@ -93,8 +93,8 @@ def run_experiment(cfg):
                 pred_liks.append(0)
             objs.append(np.mean(per_seed_obj[-50:]))
             times.append((time.time() - start)/cfg.training.num_iters)
-            writer.add_scalar('Time/Run number {}'.format(i,K), times[-1], K)
-            writer.add_scalar('Predictive Log Likelihood/Run number {}'.format(i,K), pred_liks[-1], K)
+            # writer.add_scalar('Time/Run number {}'.format(i,K), times[-1], K)
+            # writer.add_scalar('Predictive Log Likelihood/Run number {}'.format(i,K), pred_liks[-1], K)
 
             ###
             # SAVING MODELS DOESN'T WORK YET
@@ -106,7 +106,7 @@ def run_experiment(cfg):
 
         results_dict[N][M][K] = {'final_obj':np.mean(objs),'final_obj_std':np.std(objs), 'pred_likelihood':np.mean(pred_liks), 'pred_likelihood_std':np.std(pred_liks), 'objs': objs, 'pred_liks':pred_liks, 'avg_time':np.mean(times), 'std_time':np.std(times)}
 
-    file = cfg.dataset + '/results/' + cfg.model + '/' + cfg.training.inference_method + '_' + 'N{0}_M{1}.json'.format(N,M)
+    file = cfg.dataset + '/results/' + cfg.model + '/ML_{}'.format(cfg.training.ML) + '_{}_'.format(cfg.training.lr) + '_' + 'N{0}_M{1}.json'.format(N,M)
     with open(file, 'w') as f:
         json.dump(results_dict, f)
 
