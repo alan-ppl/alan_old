4a3adadce9b0201d2e731eeff4c5388fb383acc6
ThomasHeap <thomaseheap@gmail.com>
2023-02-25 11:49:34 +0000

    running scripts

---
+++ examples/fast_ml_experiments/run_movielens_M2.sh
@@ -0,0 +1,6 @@
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-1 training.ML=2 training.N=5 training.M=300
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-2 training.ML=2 training.N=5 training.M=300
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-3 training.ML=2 training.N=5 training.M=300
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-4 training.ML=2 training.N=5 training.M=300
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-5 training.ML=2 training.N=5 training.M=300
+python runner_ml.py dataset=movielens model=movielens training.pred_ll.do_pred_ll=True training.lr=1e-6 training.ML=2 training.N=5 training.M=300
