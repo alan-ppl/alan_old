e4ff24d1446de1dd163abd96bba52297525c5ccd
ThomasHeap <thomaseheap@gmail.com>
2023-05-05 15:38:55 +0100

    updates to running script

--- examples/fast_ml_experiments/run_bus_VI.sh
+++ examples/fast_ml_experiments/run_bus_VI.sh
@@ -1,9 +1,9 @@
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[3,10,30] training.num_iters=3500
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=8e-3 training.Ks=[3,10,30] training.num_iters=3500
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-3 training.Ks=[3,10,30] training.num_iters=3500
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=3e-3 training.Ks=[3,10,30] training.num_iters=3500
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[3,10,30] training.num_iters=5000
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=8e-3 training.Ks=[3,10,30] training.num_iters=5000
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-3 training.Ks=[3,10,30] training.num_iters=5000
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=3e-3 training.Ks=[3,10,30] training.num_iters=5000
 
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[3,10,30] training.num_iters=3500 use_data=False
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=8e-3 training.Ks=[3,10,30] training.num_iters=3500
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-3 training.Ks=[3,10,30] training.num_iters=3500 use_data=False
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=3e-3 training.Ks=[3,10,30] training.num_iters=3500 use_data=False
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[3,10,30] training.num_iters=5000 use_data=False
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=8e-3 training.Ks=[3,10,30] training.num_iters=5000
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-3 training.Ks=[3,10,30] training.num_iters=5000 use_data=False
+lbatch -c 1 -g 1 -m 22 -t 12 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=3e-3 training.Ks=[3,10,30] training.num_iters=5000 use_data=False
