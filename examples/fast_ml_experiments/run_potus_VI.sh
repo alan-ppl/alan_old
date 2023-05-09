597d02445f35372c9401a51012cf06e88bd55751
ThomasHeap <thomaseheap@gmail.com>
2023-05-09 19:02:49 +0100

    update to running scripts

--- examples/fast_ml_experiments/run_potus_VI.sh
+++ examples/fast_ml_experiments/run_potus_VI.sh
@@ -1,7 +1,7 @@
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=3e-1 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100]
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-1 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100]
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=3e-2 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100]
-lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-2 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100]
+lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=3e-1 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100] training.num_runs=1
+lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-1 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100] training.num_runs=1
+lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=3e-2 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100] training.num_runs=1
+lbatch -c 1 -g 1 -m 22 -t 6 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-2 training.ML=1 training.N=5 training.M=300 training.num_iters=4300 training.Ks=[3,10,30,100] training.num_runs=1
 
 # lbatch -c 1 -g 1 -m 22 -t 3 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-1 training.ML=1 training.N=5 training.M=300 training.num_iters=1000 use_data=False
 # lbatch -c 1 -g 1 -m 22 -t 3 -q cnu -a cosc020762 --cmd python runner.py dataset=potus model=potus training.lr=1e-2 training.ML=1 training.N=5 training.M=300 training.num_iters=1000 use_data=False
