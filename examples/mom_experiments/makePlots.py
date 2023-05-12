cb3c40b03b9c197c203d02eeba8dadf19dd0cdde
sambowyer <sambowyer2@gmail.com>
2023-05-12 22:52:39 +0100

    changed plot colours

--- examples/mom_experiments/makePlots.py
+++ examples/mom_experiments/makePlots.py
@@ -2,6 +2,7 @@ import json
 import matplotlib.pyplot as plt
 from tueplots import axes, bundles
 import combineJSONs
+import numpy as np
 
 def combineVIJsons(vi_lrs, experimentName, eval_k=1, dataset_seed=0):
     for metric in ["elbo", "MSE", "p_ll", "variance"]:
@@ -19,7 +20,7 @@ def combineALLJsons(experimentName, NUTS=False, dataset_seed=0):
         fs = [f"{experimentName}/results/{experimentName}_{metric}_{dataset_seed}.json",
           f"{experimentName}/results/vi_{experimentName}_{metric}_{dataset_seed}.json"]
         if NUTS:
-            fs.append(f"{experimentName}/results/hmc_{experimentName}_MINI100_{metric}_{dataset_seed}.json")
+            fs.append(f"{experimentName}/results/hmc_{experimentName}_{metric}_{dataset_seed}.json")
 
         combineJSONs.combineMultiple(fs, f"{experimentName}/results/ALL_{experimentName}_{metric}_{dataset_seed}.json")
         
@@ -81,7 +82,7 @@ def getMeanValues(data, method, Ks, rv=None, rolling_window=1):
     
 def getStdErrValues(data, method, Ks, rv=None, rolling_window=1):
     if rv is None:
-        return getSeriesValues(data, method, Ks, "std_err", rolling_window=rolling_window)
+        return [x*(1000**0.5) for x in getSeriesValues(data, method, Ks, "std_err", rolling_window=rolling_window)]
     else:
         return []
 
@@ -92,6 +93,7 @@ def getTimeValues(data, method, Ks, rv=None, rolling_window=1):
         return getSeriesValues(data, method, Ks, "time_mean", rolling_window=rolling_window)#, rv)
     
 
+greenColourRGB = (77/256, 175/256, 74/256)
 
 def plotAllForSinglePlateCombination(Ks, experimentName, rv, vi_lrs=[0.01,0.001,0.0001], vi_rolling_window=1, vi_eval_k=1, NUTS=False, dataset_seeds=[0]):
     
@@ -123,14 +125,14 @@ def plotAllForSinglePlateCombination(Ks, experimentName, rv, vi_lrs=[0.01,0.001,
             data = metricToData[metric]
             results[metric] = {}
             for method in Ks:
-                if not (method == "NUTS" and metric == "elbo"):
+                if not (method == "NUTS" and (metric == "elbo" or not NUTS)):
                     if metric in ("elbo", "p_ll"):
                         rv_temp = None
                     else:
                         rv_temp = rv
                     if "vi" in method and vi_rolling_window > 1:
                         results[metric][method] = {"mean": getMeanValues(data, method, Ks, rv_temp, vi_rolling_window),
-                                                "std_err": getStdErrValues(data, method, Ks, rv_temp, vi_rolling_window),
+                                                "std_err": getStdErrValues(data, method, Ks, rv_temp, vi_rolling_window)*np.sqrt(1000),
                                                 "time": getTimeValues(data, method, Ks, rv_temp, vi_rolling_window)}
                     else:
                         results[metric][method] = {"mean": getMeanValues(data, method, Ks, rv_temp),
@@ -148,52 +150,53 @@ def plotAllForSinglePlateCombination(Ks, experimentName, rv, vi_lrs=[0.01,0.001,
         #     results["vars"]["NUTS"] = {"mean": getMeanValues(nuts_vars, "NUTS", Ks, rv),
         #                                 "std_err": getStdErrValues(nuts_vars, "NUTS", Ks, rv),
         #                                 "time": getTimeValues(nuts_vars, "NUTS", Ks, rv)}
-        print(results["vars"]["vi_0.01"]["time"])
+
+
         fig, ax = plt.subplots(2,4,figsize=(8.5, 4.5))
         
-        ax[0,0].errorbar(Ks["tmc_new"],  results["elbo"]["tmc_new"]["mean"],  yerr=results["elbo"]["tmc_new"]["std_err"],  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[0,0].errorbar(Ks["global_k"], results["elbo"]["global_k"]["mean"], yerr=results["elbo"]["global_k"]["std_err"], linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[0,0].errorbar(Ks["tmc_new"],  results["elbo"]["tmc_new"]["mean"],  yerr=results["elbo"]["tmc_new"]["std_err"],  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[0,0].errorbar(Ks["global_k"], results["elbo"]["global_k"]["mean"], yerr=results["elbo"]["global_k"]["std_err"], linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
 
-        ax[0,1].errorbar(Ks["tmc_new"][1:],  results["p_ll"]["tmc_new"]["mean"][1:],  yerr=results["p_ll"]["tmc_new"]["std_err"][1:],  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[0,1].errorbar(Ks["global_k"][1:], results["p_ll"]["global_k"]["mean"][1:], yerr=results["p_ll"]["global_k"]["std_err"][1:], linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[0,1].errorbar(Ks["tmc_new"][1:],  results["p_ll"]["tmc_new"]["mean"][1:],  yerr=results["p_ll"]["tmc_new"]["std_err"][1:],  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[0,1].errorbar(Ks["global_k"][1:], results["p_ll"]["global_k"]["mean"][1:], yerr=results["p_ll"]["global_k"]["std_err"][1:], linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
 
-        ax[0,2].errorbar(Ks["tmc_new"][1:],  results["vars"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[0,2].errorbar(Ks["global_k"][1:], results["vars"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[0,2].errorbar(Ks["tmc_new"][1:],  results["vars"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[0,2].errorbar(Ks["global_k"][1:], results["vars"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
 
-        ax[0,3].errorbar(Ks["tmc_new"][1:],  results["MSE"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[0,3].errorbar(Ks["global_k"][1:], results["MSE"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[0,3].errorbar(Ks["tmc_new"][1:],  results["MSE"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[0,3].errorbar(Ks["global_k"][1:], results["MSE"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
 
 
-        ax[1,0].errorbar(results["elbo"]["tmc_new"]["time"],  results["elbo"]["tmc_new"]["mean"],  yerr=results["elbo"]["tmc_new"]["std_err"],  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[1,0].errorbar(results["elbo"]["global_k"]["time"], results["elbo"]["global_k"]["mean"], yerr=results["elbo"]["global_k"]["std_err"], linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[1,0].errorbar(results["elbo"]["tmc_new"]["time"],  results["elbo"]["tmc_new"]["mean"],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[1,0].errorbar(results["elbo"]["global_k"]["time"], results["elbo"]["global_k"]["mean"], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
         for alpha, lr in enumerate(vi_lrs[::-1]):
             # if not(experimentName == "bus_breakdown" and lr in [0.1, 0.01]):
-            ax[1,0].plot(results["elbo"][f"vi_{lr}"]["time"][:101], results["elbo"][f"vi_{lr}"]["mean"][:101], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(0,0,1,1/(alpha+1)))
+            ax[1,0].errorbar(results["elbo"][f"vi_{lr}"]["time"][:101], results["elbo"][f"vi_{lr}"]["mean"][:101], yerr=0, linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(*greenColourRGB, 1/(alpha+1)))
 
         # ax[1,0].set_yscale("log")
         if experimentName == "bus_breakdown":
             ax[1,0].set_ylim(-40000, 0)
 
-        ax[1,1].errorbar(results["p_ll"]["tmc_new"]["time"][1:],  results["p_ll"]["tmc_new"]["mean"][1:],  yerr=results["p_ll"]["tmc_new"]["std_err"][1:],  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[1,1].errorbar(results["p_ll"]["global_k"]["time"][1:], results["p_ll"]["global_k"]["mean"][1:], yerr=results["p_ll"]["global_k"]["std_err"][1:], linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[1,1].errorbar(results["p_ll"]["tmc_new"]["time"][1:],  results["p_ll"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[1,1].errorbar(results["p_ll"]["global_k"]["time"][1:], results["p_ll"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
         for alpha, lr in enumerate(vi_lrs[::-1]):
-            ax[1,1].plot(results["p_ll"][f"vi_{lr}"]["time"][:101], results["p_ll"][f"vi_{lr}"]["mean"][:101], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(0,0,1,1/(alpha+1)))
+            ax[1,1].errorbar(results["p_ll"][f"vi_{lr}"]["time"][:101], results["p_ll"][f"vi_{lr}"]["mean"][:101], yerr=0, linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(*greenColourRGB, 1/(alpha+1)))
         if NUTS:
-            ax[1,1].plot(results["p_ll"]["NUTS"]["time"], results["p_ll"]["NUTS"]["mean"], linewidth=0.55, markersize = 0.75, label="NUTS", c='black')
+            ax[1,1].errorbar(results["p_ll"]["NUTS"]["time"], results["p_ll"]["NUTS"]["mean"], yerr=0, linewidth=0.55, markersize = 0.75, label="NUTS", c='#984ea3')
 
-        ax[1,2].errorbar(results["vars"]["tmc_new"]["time"][1:],  results["vars"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[1,2].errorbar(results["vars"]["global_k"]["time"][1:], results["vars"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[1,2].errorbar(results["vars"]["tmc_new"]["time"][1:],  results["vars"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[1,2].errorbar(results["vars"]["global_k"]["time"][1:], results["vars"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
         for alpha, lr in enumerate(vi_lrs[::-1]):
-            ax[1,2].plot(results["vars"][f"vi_{lr}"]["time"][:101], results["vars"][f"vi_{lr}"]["mean"][:101], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(0,0,1,1/(alpha+1)))
+            ax[1,2].plot(results["vars"][f"vi_{lr}"]["time"][:101], results["vars"][f"vi_{lr}"]["mean"][:101], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(*greenColourRGB, 1/(alpha+1)))
         if NUTS:
-            ax[1,2].plot(results["vars"]["NUTS"]["time"], results["vars"]["NUTS"]["mean"], linewidth=0.55, markersize = 0.75, label="NUTS", c='black')
+            ax[1,2].plot(results["vars"]["NUTS"]["time"], results["vars"]["NUTS"]["mean"], linewidth=0.55, markersize = 0.75, label="NUTS", c='#984ea3')
 
-        ax[1,3].errorbar(results["MSE"]["tmc_new"]["time"][1:],  results["MSE"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='red', label='MP IS')
-        ax[1,3].errorbar(results["MSE"]["global_k"]["time"][1:], results["MSE"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='green', label='Global IS')
+        ax[1,3].errorbar(results["MSE"]["tmc_new"]["time"][1:],  results["MSE"]["tmc_new"]["mean"][1:],  yerr=0,  linewidth=0.55, markersize = 0.75, fmt='-o', c='#e41a1c', label='MP IS')
+        ax[1,3].errorbar(results["MSE"]["global_k"]["time"][1:], results["MSE"]["global_k"]["mean"][1:], yerr=0, linewidth=0.55, markersize = 0.75, fmt='-o', c='#377eb8', label='Global IS')
         for alpha, lr in enumerate(vi_lrs[::-1]):
-            ax[1,3].plot(results["MSE"][f"vi_{lr}"]["time"], results["MSE"][f"vi_{lr}"]["mean"], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(0,0,1,1/(alpha+1)))
+            ax[1,3].plot(results["MSE"][f"vi_{lr}"]["time"], results["MSE"][f"vi_{lr}"]["mean"], linewidth=0.55, markersize = 0.75, label="VI lr="+str(lr), c=(*greenColourRGB, 1/(alpha+1)))
         if NUTS:
-            ax[1,3].plot(results["MSE"]["NUTS"]["time"], results["MSE"]["NUTS"]["mean"], linewidth=0.55, markersize = 0.75, label="NUTS", c='black')
+            ax[1,3].plot(results["MSE"]["NUTS"]["time"], results["MSE"]["NUTS"]["mean"], linewidth=0.55, markersize = 0.75, label="NUTS", c='#984ea3')
 
         if rv == "alpha": rv = "BoroughMean"  # change to match the name of the variable in the paper
 
@@ -226,8 +229,8 @@ def plotAllForSinglePlateCombination(Ks, experimentName, rv, vi_lrs=[0.01,0.001,
         for i in range(2):
             ax[i,0].set_ylabel("Evidence Lower Bound", fontsize=10)
             ax[i,1].set_ylabel("Predictive Log-Likelihood", fontsize=10)
-            ax[i,2].set_ylabel(f"Variance of {rv} Estimator", fontsize=10)
-            ax[i,3].set_ylabel(f"MSE of {rv} Estimator", fontsize=10)
+            ax[i,2].set_ylabel(f"Variance of \n{rv} Estimator", fontsize=10)
+            ax[i,3].set_ylabel(f"MSE of \n{rv} Estimator", fontsize=10)
 
         plt.legend(loc="center right")
         plt.savefig(f'plots/{experimentName}_all_{rv}.png')
@@ -260,7 +263,7 @@ num_vi_iters = 100#200
 vi_iter_step = 1
 vi_iter_counts = [str(x) for x in range(0, num_vi_iters+1, vi_iter_step)]
 
-num_NUTS_samples = 25
+num_NUTS_samples = 5
 
 Ks = {"tmc_new": ['1', '3', '10', '30', '100'], "global_k": ['1', '3','10','30', '100', '300', '1000', '3000', '10000', '30000', '100000']}
 
@@ -269,5 +272,5 @@ for lr in vi_lrs:
 
 Ks["NUTS"] = [str(x) for x in range(1, num_NUTS_samples+1)]
 
-# plotAllForSinglePlateCombination(Ks, "bus_breakdown", "alpha", vi_lrs=vi_lrs, NUTS=False)
+plotAllForSinglePlateCombination(Ks, "bus_breakdown", "alpha", vi_lrs=vi_lrs, NUTS=True)
 
