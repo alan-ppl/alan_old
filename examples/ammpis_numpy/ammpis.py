6f75ea4f27443d034e0b1ce5cf768ddee3a80154
ThomasHeap <thomaseheap@gmail.com>
2023-07-28 13:36:54 +0100

    new experiment script

--- examples/ammpis_numpy/ammpis.py
+++ examples/ammpis_numpy/ammpis.py
@@ -65,6 +65,7 @@ def IW(sample, approx_dist, post):
 
     weights = weights / weights.sum(axis=0)
     
+
     Ex_one_iter = (weights * sample).sum(axis=0)
     Ex2_one_iter = (weights * sample**2).sum(axis=0)
     m_one_iter = t.stack([Ex_one_iter, Ex2_one_iter]).t()
@@ -124,19 +125,20 @@ def ammp_is(T, post_params, init_moments, lr, K=5, approx_post_type=Normal, post
             #log_w_t = -ReLu(H_Q - H_Q_temp)
 
             
-            dt = log_w_t - log_w_t_minus_one
+            dt = log_w_t - log_w_t_minus_one #- 0.1
 
-            l_tot_t = l_tot[-1] + dt + softplus(l_one_iter_t - dt - l_tot[-1])
+            l_tot_t = l_tot[-1] - dt + softplus(l_one_iter_t + dt - l_tot[-1])
             eta_t = np.exp(l_one_iter_t - l_tot_t)
           
             new_m_avg = eta_t * m_one_iter_t + (1 - eta_t) * m_avg[-1]
 
             Q_temp_params = fit_approx_post(new_m_avg, approx_post_type)
 
+
             Q_temp = approx_post_type(Q_temp_params[:,0], Q_temp_params[:,1])
             H_Q_temp = entropy(Q_temp)
 
-        weights.append(dt.clone())
+        weights.append(log_w_t.clone())
         l_one_iters.append(l_one_iter_t.clone())
         l_tot.append(l_tot_t.clone())
         m_avg.append(new_m_avg.clone())
@@ -529,117 +531,8 @@ if __name__ == "__main__":
     # breakpoint()
 
     post_params = t.cat([loc, scale], dim=1)
-    # breakpoint()
 
-    colors = ['#a6611a','#dfc27d','#80cdc1','#018571']
-    k = 0
-
-    fig, ax = plt.subplots(4,1, figsize=(5.5, 12.0))
-
-
-    for fn in [ammp_is, ammp_is_uniform_dt, ammp_is_no_inner_loop, ammp_is_weight_all]:
-        m_q, m_avg, l_tot, l_one_iters, log_weights, entropies, ammp_is_times = fn(500, post_params, init, 0.4, K)
-        mean_errs, var_errs = get_errs(m_q, post_params)
-
-        print(f"Final ELBO {fn.__name__}: ", l_tot[-1])
-
-        ax[0].plot(mean_errs, c=colors[k], label=f'{fn.__name__}')
-        ax[0].set_title("Mean Error")
-
-        ax[1].plot(var_errs, c=colors[k], label=f'{fn.__name__}')
-        ax[1].set_title("Var Error")
-
-        ax[2].plot([lt + lw for lt, lw in zip(l_one_iters, log_weights)][2:], c=colors[k], label=f'{fn.__name__}')
-        ax[2].set_title("weighted l_one_iter")
-
-        ax[3].plot(entropies, c=colors[k], label=f'{fn.__name__}')
-        ax[3].set_title("Entropy")
-        k += 1
-
-
-    # vi_means, vi_vars, elbos, entropies, vi_times = VI(500, post_params, init, 0.4, K=K)
-
-    # mean_errs = []
-    # var_errs = []
-
-    # for i in range(len(vi_means)):
-    #     mean_errs.append((post_params[:,0] - vi_means[i]).abs().mean())
-    #     var_errs.append((post_params[:,1] - vi_vars[i].exp()).abs().mean())
-
-    # ax[0].plot(mean_errs, c='#54278f', label='VI')
-    # ax[1].plot(var_errs, c='#54278f', label='VI')
-    # ax[2].plot(elbos, c='#54278f', label='VI') 
-    # ax[3].plot(entropies, c='#54278f', label='VI')
-
-
-    hmc_moms, hmc_times, hmc_samples = HMC(125, post_params, init, post_type=Normal, num_chains=4)
-
-    mean_errs, var_errs = get_errs(hmc_moms, post_params)
-    ax[0].plot(mean_errs, c='#FF10F0', label='HMC')
-    ax[1].plot(var_errs, c='#FF10F0', label='HMC')
-
-    
-    m_mcmc, mcmc_acceptance_rate, mcmc_times, mcmc_samples = mcmc(500, post_params, init, 2.4*scale, burn_in=100)
-    mean_errs_mcmc, var_errs_mcmc = get_errs(m_mcmc, post_params)
-    print("MCMC acceptance rate: ", mcmc_acceptance_rate)  # should be 0.44 for Gaussian posterior
-
-
-    m_lang, lang_acceptance_rate, lang_times, lang_samples = lang(500, post_params, init, 2.4*scale, burn_in=100)
-    mean_errs_lang, var_errs_lang = get_errs(m_lang, post_params)
-    print("Lang acceptance rate: ", lang_acceptance_rate)  # should be 0.574 for Gaussian posterior
-
-    ax[0].plot(mean_errs_mcmc, c='b', label='MCMC')
-    ax[1].plot(var_errs_mcmc, c='b', label='MCMC')
-
-    ax[0].plot(mean_errs_lang, c='r', label='Lang')
-    ax[1].plot(var_errs_lang, c='r', label='Lang')
-
-    # log y axis
-    ax[0].set_yscale('log')
-    ax[1].set_yscale('log')
-    ax[2].set_yscale('symlog')
-    ax[3].set_yscale('log')
-
-    ax[0].legend(loc='upper right')
-    plt.tight_layout()  
-    plt.savefig('figures/ammp_is_vs_mcmc.png')
-
-
-    # #Posterior mean and scale error
-    # print("Posterior mean error: ", (post_params[:,0] - fit_approx_post(m_q[-1])[:,0]).abs().mean())
-    # print("Posterior scale error: ", (post_params[:,1] - fit_approx_post(m_q[-1])[:,1]).abs().mean())
-
-    # print("MCMC mean error: ", (post_params[:,0] - fit_approx_post(m_mcmc[-1])[:,0]).abs().mean())
-    # print("MCMC scale error: ", (post_params[:,1] - fit_approx_post(m_mcmc[-1])[:,1]).abs().mean())
-
-    # print("Lang mean error: ", (post_params[:,0] - fit_approx_post(m_lang[-1])[:,0]).abs().mean())
-    # print("Lang scale error: ", (post_params[:,1] - fit_approx_post(m_lang[-1])[:,1]).abs().mean())
-
-    # breakpoint()
-
-    # print('Mean')
-    # print(post_params[:,0])
-    # print(fit_approx_post(m_q[-1])[:,0])
-    # print(fit_approx_post(m_mcmc[-1])[:,0])
-
-
-    # print('Scale')
-    # print(post_params[:,1])
-    # print(fit_approx_post(m_q[-1])[:,1])   
-    # print(fit_approx_post(m_mcmc[-1])[:,1])
-
-    # Want to tune MCMC to have acceptance rate of 0.44
-    MCMC_SCALE_TEST = False
-    if MCMC_SCALE_TEST:
-        for proposal_scale in [2.4*scale, 2.4, 2.4/num_latents**0.5, 2.4*scale/num_latents**0.5, 2.4]:
-            # N.B. 2.4*scale SHOULD be optimal (in Gaussian posterior case)
-            m_mcmc, mcmc_acceptance_rate, mcmc_times, mcmc_samples = mcmc(10000, post_params, init, proposal_scale, burn_in=1000)
-
-            print("Proposal sigma: ", proposal_scale)
-            print("MCMC mean error: ", (post_params[:,0] - fit_approx_post(m_mcmc[-1])[:,0]).abs().mean())
-            print("MCMC scale error: ", (post_params[:,1] - fit_approx_post(m_mcmc[-1])[:,1]).abs().mean())
-            print("MCMC acceptance rate: ", mcmc_acceptance_rate)
-            print()
+    m_q, m_avg, l_tot, l_one_iters, log_weights, entropies, ammp_is_times = ammp_is(1000, post_params, init, 0.4, K)
 
     
         
--- examples/ammpis_numpy/experiments.py
+++ examples/ammpis_numpy/experiments.py
@@ -11,206 +11,134 @@ from torch.distributions import Normal, Laplace, Gumbel
 t.manual_seed(0)
 t.cuda.manual_seed(0)
 
-if __name__ == "__main__":
-    num_runs = 1
 
-    num_iters = 100 # mcmc and lang seem to require about 1000 iterations to converge
-    
-    num_latents = [10, 100, 1000]
-    
+num_latents = 500
+K=5
+init = t.tensor([0.0,1.0], dtype=t.float64).repeat((num_latents,1))
 
-    posteriors = {"Normal": Normal, "Laplace": Laplace, "Gumbel": Gumbel}
+loc = Normal(0,1).sample((num_latents,1)).float()
+# scale = Normal(0,1).sample((num_latents,1)).exp().float()
+scale = Uniform(-5,-0.5).sample((num_latents,1)).exp().float()
 
-    Ks = [1,3,10,30,100]
-        
-    # ammp_is_fns = [ammp_is, ammp_is_uniform_dt, ammp_is_no_inner_loop, ammp_is_weight_all]
-    ammp_is_fns = [ammp_is, ammp_is_weight_all]
-    errors = {"ammp_is_post_q":   t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_post_avg": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_uniform_dt_post_q":   t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_uniform_dt_post_avg": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_no_inner_loop_post_q":   t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_no_inner_loop_post_avg": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_weight_all_post_q":   t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "ammp_is_weight_all_post_avg": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "mcmc_post":        t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2),
-              "lang_post":        t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1, 2)}
+# print(scale)
+# breakpoint()
+print(f'Location mean: {loc.abs().mean()}')
+print(f'Variance mean: {scale.mean()}')
+post_params = t.cat([loc, scale], dim=1)
+# breakpoint()
+
+colors = ['#a6611a','#dfc27d','#80cdc1','#018571']
+k = 0
+
+fig, ax = plt.subplots(4,1, figsize=(5.5, 12.0))
 
 
+for fn in [ammp_is, ammp_is_uniform_dt]:# ammp_is_no_inner_loop, ammp_is_weight_all]:
+    m_q, m_avg, l_tot, l_one_iters, log_weights, entropies, ammp_is_times = fn(2000, post_params, init, 0.05, K)
+    mean_errs, var_errs = get_errs(m_q, post_params)
 
-    entropies = {"ammp_is": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-            "ammp_is_uniform_dt": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-            "ammp_is_no_inner_loop": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-            "ammp_is_weight_all": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-            "mcmc":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-            "lang":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1)}
-    
-    weighted_ltots = {"ammp_is": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-        "ammp_is_uniform_dt": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-        "ammp_is_no_inner_loop": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-        "ammp_is_weight_all": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-        "mcmc":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-        "lang":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1)}
-    
+    print(f"Final mean of approx mean {fn.__name__}: ", fit_approx_post(m_q[-1])[:,0].abs().mean())
+    print(f"Final mean of approx var {fn.__name__}: ", fit_approx_post(m_q[-1])[:,1].mean())
+    print(f"Final mean error {fn.__name__}: ", mean_errs[-1])
+    print(f"Final var error {fn.__name__}: ", var_errs[-1])
+    print(f"Final ELBO {fn.__name__}: ", l_tot[-1])
 
-    times = {"ammp_is": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-             "ammp_is_uniform_dt": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-             "ammp_is_no_inner_loop": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-             "ammp_is_weight_all": t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-             "mcmc":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1),
-             "lang":    t.zeros(num_runs, len(Ks), len(num_latents), num_iters+1)} 
-    
-    for run in range(num_runs):
-        for K in Ks:
-            for n in num_latents:
-                print(f"run: {run}, K: {K}, n: {n}", end="\t")
-            
-                init = t.tensor([0.0,1.0], dtype=t.float64).repeat((n,1))
+    ax[0].plot(mean_errs, c=colors[k], label=f'{fn.__name__}')
+    ax[0].set_title("Mean Error")
 
-                # priors on posterior parameters
-                loc = Normal(0,100).sample((n,1)).float()
-                # scale = Normal(0,0.1).sample((n,1)).exp().float() + 5
-                scale = Uniform(-5,-0.5).sample((n,1)).exp().float()
-                # do log uniform instead of uniform to avoid numerical issues
+    ax[1].plot(var_errs, c=colors[k], label=f'{fn.__name__}')
+    ax[1].set_title("Var Error")
 
-                post_params = t.cat([loc, scale], dim=1)
-                post_dist = Normal(loc, scale)
-                true_post = post_params
+    ax[2].plot([lt + lw for lt, lw in zip(l_one_iters, log_weights)][2:], c=colors[k], label=f'{fn.__name__}')
+    ax[2].set_title("weighted l_one_iter")
 
-                final_post_avg = {}
-
-                for fn in ammp_is_fns:
-
-                    name = fn.__name__
-                    # ammpis
-                    m_q, m_avg, l_tot, l_one_iters, weights, ents, fn_times = fn(num_iters, post_params, init, 0.4, K)
-                    post_q = [fit_approx_post(m) for m in m_q]
-                    post_avg = [fit_approx_post(m) for m in m_avg]
-
-                    final_post_avg[name] = post_avg[-1]
-
-                    # calculate errors
-                    for i in range(num_iters+1):
-                        errors[f"{name}_post_q"][run, Ks.index(K), num_latents.index(n), i, :] = (true_post - post_q[i]).abs().mean(0)
-                        errors[f"{name}_post_avg"][run, Ks.index(K), num_latents.index(n), i, :] = (true_post - post_avg[i]).abs().mean(0)
-
-                    times[f"{name}"][run, Ks.index(K), num_latents.index(n), :] = fn_times
-                    entropies[f"{name}"][run, Ks.index(K), num_latents.index(n), :] = t.tensor(ents)
-
-                    weighted_ltots[f"{name}"][run, Ks.index(K), num_latents.index(n), :] = t.tensor([lt + wt for lt, wt in zip(l_tot, weights)])
+    ax[3].plot(entropies, c=colors[k], label=f'{fn.__name__}')
+    ax[3].set_title("Entropy")
+    k += 1
 
 
+# vi_means, vi_vars, elbos, entropies, vi_times = VI(500, post_params, init, 0.4, K=K)
 
-                # mcmc
-                burn_in = num_iters//10
-                m_mcmc, mcmc_acceptance_rate, mcmc_times, mcmc_samples = mcmc(num_iters, post_params, init, 2.4*scale, burn_in=burn_in)
-                mcmc_post = [fit_approx_post(m) for m in m_mcmc]
+# mean_errs = []
+# var_errs = []
 
-                # langevin (mala)
-                m_lang, lang_acceptance_rate, lang_times, lang_samples = lang(num_iters, post_params, init, 2.4*scale, burn_in=burn_in)
-                lang_post = [fit_approx_post(m) for m in m_lang]
+# for i in range(len(vi_means)):
+#     mean_errs.append((post_params[:,0] - vi_means[i]).abs().mean())
+#     var_errs.append((post_params[:,1] - vi_vars[i].exp()).abs().mean())
 
-                print(f"Acceptance rates: mcmc={mcmc_acceptance_rate},\t lang={lang_acceptance_rate}")
-
-                # calculate errors
-                for i in range(num_iters+1):
-                    errors["mcmc_post"][run, Ks.index(K), num_latents.index(n), i, :] = (true_post - mcmc_post[i]).abs().mean(0)
-                    errors["lang_post"][run, Ks.index(K), num_latents.index(n), i, :] = (true_post - lang_post[i]).abs().mean(0)
-
-                times["mcmc"][run, Ks.index(K), num_latents.index(n), :] = mcmc_times
-                times["lang"][run, Ks.index(K), num_latents.index(n), :] = lang_times
-
-                # # plot the pdfs of the final approximating distributions
-                # if run == 0: # and post == "Gumbel" and n != 1000:
-                #     fig, ax = plt.subplots(1,1, figsize=(5.5, 4.0))
-                #     # breakpoint()
-
-                #     df = pd.DataFrame({"true": post_dist.sample((num_iters*10,)).numpy()[:,0][:,0],
-                #                        "mcmc": mcmc_samples[burn_in:,0].repeat(10),
-                #                        "lang": lang_samples[burn_in:,0].repeat(10)})
-                #                         # "mcmc": Normal(mcmc_post[-1][:,0], mcmc_post[-1][:,1]).sample((num_iters*10,)).numpy()[:,0],
-                #                         # "lang": Normal(lang_post[-1][:,0], lang_post[-1][:,1] + 1e-10).sample((num_iters*10,)).numpy()[:,0]})
-
-                #     for name in final_post_avg:
-                #         df[name] = Normal(final_post_avg[name][:,0], final_post_avg[name][:,1]).sample((num_iters*10,)).numpy()[:,0]
+# ax[0].plot(mean_errs, c='#54278f', label='VI')
+# ax[1].plot(var_errs, c='#54278f', label='VI')
+# ax[2].plot(elbos, c='#54278f', label='VI') 
+# ax[3].plot(entropies, c='#54278f', label='VI')
 
 
-                #     df.plot.kde(ax=ax, legend=True, ind=None)
+hmc_moms, hmc_times, hmc_samples = HMC(125, post_params, init, post_type=Normal, num_chains=4)
 
-                #     ax.set_title(f"n={n}, {K}({loc[0,0].item():.3f}, {scale[0,0].item():.3f}) posterior")
-                #     plt.savefig(f"figures/{K}_n{n}_run{run}.png")
-                #     plt.close()
-
-
-    # take mean over runs
-    for fn in ammp_is_fns:
-        name = fn.__name__
-        errors[f"{name}_post_q"] = errors[f"{name}_post_q"].mean(0)
-        errors[f"{name}_post_avg"] = errors[f"{name}_post_avg"].mean(0)
-        times[f"{name}"] = times[f"{name}"].mean(0)
-        entropies[f"{name}"] = entropies[f"{name}"].mean(0)
-        weighted_ltots[f"{name}"] = weighted_ltots[f"{name}"].mean(0)
+mean_errs, var_errs = get_errs(hmc_moms, post_params)
+ax[0].plot(mean_errs, c='#FF10F0', label='HMC')
+ax[1].plot(var_errs, c='#FF10F0', label='HMC')
 
 
-
-    errors["mcmc_post"] = errors["mcmc_post"].mean(0)
-    errors["lang_post"] = errors["lang_post"].mean(0)
-
-
-    times["mcmc"] = times["mcmc"].mean(0)
-    times["lang"] = times["lang"].mean(0)
+m_mcmc, mcmc_acceptance_rate, mcmc_times, mcmc_samples = mcmc(500, post_params, init, 2.4*scale, burn_in=100)
+mean_errs_mcmc, var_errs_mcmc = get_errs(m_mcmc, post_params)
+print("MCMC acceptance rate: ", mcmc_acceptance_rate)  # should be 0.44 for Gaussian posterior
 
 
+m_lang, lang_acceptance_rate, lang_times, lang_samples = lang(500, post_params, init, 2.4*scale, burn_in=100)
+mean_errs_lang, var_errs_lang = get_errs(m_lang, post_params)
+print("Lang acceptance rate: ", lang_acceptance_rate)  # should be 0.574 for Gaussian posterior
+
+ax[0].plot(mean_errs_mcmc, c='b', label='MCMC')
+ax[1].plot(var_errs_mcmc, c='b', label='MCMC')
+
+ax[0].plot(mean_errs_lang, c='r', label='Lang')
+ax[1].plot(var_errs_lang, c='r', label='Lang')
+
+# log y axis
+ax[0].set_yscale('log')
+ax[1].set_yscale('log')
+# ax[2].set_yscale('symlog')
+# ax[3].set_yscale('symlog')
+
+ax[2].set_ylim(-100, 20)
+
+ax[0].legend(loc='upper right')
+plt.tight_layout()  
+plt.savefig('figures/ammp_is_vs_mcmc.png')
 
 
-    for x_axis in ["iterations", "time"]:
-        for K in Ks:
-            # subplot for each num_latents and each element of the error tensor (mean, scale)
-            fig, axs = plt.subplots(len(num_latents), 4, figsize=(16,20), sharex=(x_axis=="iterations"))
-            fig.suptitle(f"K={K}", x=0.17, y=0.95, weight="bold")
+# #Posterior mean and scale error
+# print("Posterior mean error: ", (post_params[:,0] - fit_approx_post(m_q[-1])[:,0]).abs().mean())
+# print("Posterior scale error: ", (post_params[:,1] - fit_approx_post(m_q[-1])[:,1]).abs().mean())
 
-            for fn in ammp_is_fns:
-                name = fn.__name__
-                for i in range(len(num_latents)):
-                    if x_axis == "iterations":
-                        xs = t.arange(num_iters+1)
-                    elif x_axis == "time":
-                        xs = times[f"{name}"][Ks.index(K), i, :]
+# print("MCMC mean error: ", (post_params[:,0] - fit_approx_post(m_mcmc[-1])[:,0]).abs().mean())
+# print("MCMC scale error: ", (post_params[:,1] - fit_approx_post(m_mcmc[-1])[:,1]).abs().mean())
 
-                    if name in ['mcmc', 'lang']:
-                        axs[i, 0].plot(xs, errors[f"{name}_post"][Ks.index(K), i, :, 0].numpy(), label=f"{name}")
-                        axs[i, 1].plot(xs, errors[f"{name}_post"][Ks.index(K), i, :, 1].numpy(), label=f"{name}")
-                    else:
-                        axs[i, 0].plot(xs, errors[f"{name}_post_q"][Ks.index(K), i, :, 0].numpy(), label=f"{name}_Qt")
-                        axs[i, 0].plot(xs, errors[f"{name}_post_avg"][Ks.index(K), i, :, 0].numpy(), label=f"{name}_av")
+# print("Lang mean error: ", (post_params[:,0] - fit_approx_post(m_lang[-1])[:,0]).abs().mean())
+# print("Lang scale error: ", (post_params[:,1] - fit_approx_post(m_lang[-1])[:,1]).abs().mean())
 
-                        axs[i, 1].plot(xs, errors[f"{name}_post_q"][Ks.index(K), i, :, 1].numpy(), label=f"{name}_Qt")
-                        axs[i, 1].plot(xs, errors[f"{name}_post_avg"][Ks.index(K), i, :, 1].numpy(), label=f"{name}_av")
+# breakpoint()
 
-                        axs[i, 2].plot(xs, entropies[f"{name}"][Ks.index(K), i, :].numpy(), label=f"{name}")
-                        axs[i, 3].plot(xs[2:], weighted_ltots[f"{name}"][Ks.index(K), i, :].numpy()[2:], label=f"{name}")
-                
-                axs[0, 0].set_title(f"Error in mean")
-                axs[0, 1].set_title("Error in variance")
-                axs[0, 2].set_title("Entropy")
-                axs[0, 3].set_title("Weighted l_tot")
+# print('Mean')
+# print(post_params[:,0])
+# print(fit_approx_post(m_q[-1])[:,0])
+# print(fit_approx_post(m_mcmc[-1])[:,0])
 
 
-                # add x-axis labels
-                for ax in axs[-1,:]:
-                    ax.set_xlabel("Iterations" if x_axis == "iterations" else "Time (s)")
+# print('Scale')
+# print(post_params[:,1])
+# print(fit_approx_post(m_q[-1])[:,1])   
+# print(fit_approx_post(m_mcmc[-1])[:,1])
 
-                # for each row, add a label to the y-axis containing the num_latents
-                for ax, row in zip(axs[:,0], num_latents):
-                    ax.set_ylabel(f"n={row}")
+# Want to tune MCMC to have acceptance rate of 0.44
+MCMC_SCALE_TEST = False
+if MCMC_SCALE_TEST:
+    for proposal_scale in [2.4*scale, 2.4, 2.4/num_latents**0.5, 2.4*scale/num_latents**0.5, 2.4]:
+        # N.B. 2.4*scale SHOULD be optimal (in Gaussian posterior case)
+        m_mcmc, mcmc_acceptance_rate, mcmc_times, mcmc_samples = mcmc(10000, post_params, init, proposal_scale, burn_in=1000)
 
-                # for ax in axs:
-                #     for a in ax:
-                #         a.set_yscale("symlog")
-
-            # add horizontal legend along top of plot
-            axs[0,1].legend(loc='lower center', bbox_to_anchor=(0.27, 1.25), shadow=False, ncol=2)
-
-            plt.savefig(f"figures/{K}{'_vs_time' if x_axis == 'time' else ''}.png")
-            plt.close()
-
+        print("Proposal sigma: ", proposal_scale)
+        print("MCMC mean error: ", (post_params[:,0] - fit_approx_post(m_mcmc[-1])[:,0]).abs().mean())
+        print("MCMC scale error: ", (post_params[:,1] - fit_approx_post(m_mcmc[-1])[:,1]).abs().mean())
+        print("MCMC acceptance rate: ", mcmc_acceptance_rate)
+        print()
