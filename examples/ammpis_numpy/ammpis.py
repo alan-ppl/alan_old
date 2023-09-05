Fixed elbo & elf computation. Now ToyML2 (source-term trick ML) matches natural_rws

examples/ammpis_numpy/ammpis.py
@@ -88,25 +88,11 @@ def IW(sample, approx_dist, post=None, prior=None, likelihood=None, elf=t.tensor
     N = sample.shape[1]
     
     # breakpoint()
-    elbo = t.logsumexp((logp.sum(dim=1) - logq.sum(dim=1) + elf), dim=0).sum() - math.log(K) # this is the incorrect version (we think) -- not sure how to reshape/resolve elf addition issues
-    # elbo = t.logsumexp(logp - logq + elf, dim=0).sum() - N*math.log(K)
-    # elbo = t.logsumexp(logp - logq + t.t(elf.unsqueeze(0)), dim=0).sum() - N*math.log(K)
+    # elbo = t.logsumexp((logp.sum(dim=1) - logq.sum(dim=1) + elf), dim=0).sum() - math.log(K) # this is the old incorrect version
+    elbo = t.logsumexp(logp - logq + elf, dim=0).sum() - N*math.log(K)
 
-    # """ zoom meeting chat
-    # logp.shape = [K, N]
-    # t.logsumexp(logp-logq, dim=0).sum()
-    # """"
+    # print("elbo: ", elbo.item())
 
-    # print(sample)
-    # print('lp ml1toy')
-    # print(t.logsumexp((logp.sum(dim=1) - logq.sum(dim=1) + elf), dim=0).sum())
-    # print(t.logsumexp((logp - logq), dim=0).sum())
-    # print(t.logsumexp((logp - logq + elf), dim=0).sum())
-    # print(t.logsumexp((logp - logq + elf), dim=0).sum() - math.log(K))
-    # print(t.logsumexp((logp - logq + elf), dim=0).sum() - N*math.log(K))
-    print("elbo: ", elbo.item())
-    # print(elf)
-    # input()
     lqp = logp - logq
     lqp_max = lqp.amax(axis=0)
     weights = t.exp(lqp - lqp_max)
@@ -485,8 +471,8 @@ def ml2(T, init_moments, lr, K=5, prior_params=None, lik_params=None, post_param
 
         z_t = sample(Q_t, K)
 
-        elf = sum((J*f(z_t)).sum(dim=1) for J,f in zip((J_loc, J_scale),(identity, t.square)))
-        # breakpoint()
+        # elf = sum((J*f(z_t)).sum(dim=1) for J,f in zip((J_loc, J_scale),(identity, t.square)))
+        elf = sum((J*f(z_t)) for J,f in zip((J_loc, J_scale),(identity, t.square)))
 
         if prior_params is not None and lik_params is not None:
             likelihood = like_type(z_t, lik_params).log_prob(data)

examples/ammpis_numpy/ml_diagnostic.py
@@ -15,12 +15,16 @@ parser.add_argument('-N', type=int, default=3)
 parser.add_argument('-K', type=int, default=3)
 parser.add_argument('-T', type=int, default=100)
 parser.add_argument('-p', '--plot_ML2_only', default=False, action='store_true')
+parser.add_argument('-b', '--backup_plots',  default=False, action='store_true')
+parser.add_argument('-s', '--seed', type=int, default=0)
 args = parser.parse_args()
 
 N = args.N
 K = args.K
 T = args.T
 plot_ML2_only = args.plot_ML2_only
+backup_plots = args.backup_plots
+seed = args.seed
 
 with open('m_mismatch_count.txt', 'w') as f:
     pass
@@ -32,7 +36,7 @@ colours = ['#9e9ac8','#fbb4b9','#253494','#de2d26','#31a354']
 
 fig_iters, ax_iters = plt.subplots(5,1, figsize=(8.0, 25.0))
 
-seed_torch(0)
+seed_torch(seed)
 
 dim_latent = N
 # K=3
@@ -40,21 +44,29 @@ dim_latent = N
 # T=100
 prior_mean = Normal(0,150).sample((dim_latent,1)).float()
 prior_scale = Uniform(1, 2).sample((dim_latent,1)).float()
-
 lik_scale = Uniform(1, 2).sample((dim_latent,)).float()
+
+# # uncomment to use plate of size N rather than N fully distinct latent variables
+# prior_mean = t.zeros((N,1))
+# prior_scale = t.ones((N,1))
+# lik_scale = t.ones(N)
+
 def P(tr):
-  '''
-  Bayesian Gaussian Model
-  '''
-  tr('mu', alan.Normal(prior_mean.squeeze(1), prior_scale.squeeze(1)))
-  tr('obs', alan.Normal(tr['mu'], lik_scale))
+    '''
+    Bayesian Gaussian Model
+    '''
+    tr('mu', alan.Normal(prior_mean.squeeze(1), prior_scale.squeeze(1)))
+    tr('obs', alan.Normal(tr['mu'], lik_scale))
 
-
+    # # use below lines instead of the above for plated model
+    # tr('mu', alan.Normal(0, 1), plates="plate1")
+    # tr('obs', alan.Normal(tr['mu'], 1))
 
 class Q_ml1(alan.AlanModule):
     def __init__(self):
         super().__init__()
         self.mu = alan.MLNormal(sample_shape=(dim_latent,))
+        # self.mu = alan.MLNormal(platesizes={"plate1": N})
 
     def forward(self, tr):
         tr('mu', self.mu())
@@ -63,12 +75,14 @@ class Q_ml2(alan.AlanModule):
     def __init__(self):
         super().__init__()
         self.mu = alan.ML2Normal(sample_shape=(dim_latent,))
+        # self.mu = alan.ML2Normal(platesizes={"plate1": N})
 
     def forward(self, tr):
         tr('mu', self.mu())
 
 #Posterior
 data = alan.Model(P).sample_prior(varnames='obs')
+# data = alan.Model(P).sample_prior(varnames='obs', platesizes={"plate1": N})
 
 # prior_scale = t.square(prior_scale)
 # lik_scale = t.square(lik_scale)
@@ -86,19 +100,19 @@ lik_params = lik_scale
 init = t.tensor([0.0,1.0], dtype=t.float64).repeat((dim_latent,1))
 
 # lr = lambda i: ((i + 10)**(-0.9))
-seed_torch(0)
+seed_torch(seed)
 m_q, l_one_iters, entropies, times = natural_rws(T, init, 0.01, K, prior_params=prior_params, lik_params=lik_params, data=data['obs'].rename(None))
 print("Natural RWS done.\n")
 
-seed_torch(0)
+seed_torch(seed)
 m_q_ml1, l_one_iters_ml1, entropies, times = ml1(T, init, 0.01, K, prior_params=prior_params, lik_params=lik_params, data=data['obs'].rename(None))
 print("ML1 Toy done.\n")
 
-seed_torch(0)
+seed_torch(seed)
 m_q_ml2, l_one_iters_ml2, entropies, times = ml2(T, init, 0.01, K, prior_params=prior_params, lik_params=lik_params, data=data['obs'].rename(None))
 print("ML2 Toy done.\n")
 
-seed_torch(0)
+seed_torch(seed)
 q = Q_ml1()
 m1 = alan.Model(P, q).condition(data=data)
 
@@ -114,7 +128,7 @@ for i in range(T):
 
 print("ML1 done.\n")
 
-seed_torch(0)
+seed_torch(seed)
 q = Q_ml2()
 m2 = alan.Model(P, q).condition(data=data)
 
@@ -157,30 +171,39 @@ ax_iters[2].legend()
 if not plot_ML2_only:
     ax_iters[3].plot([((m_q[i] - m_q_ml1[i])**2).sum() for i in range(T)], color=colours[1], label=f'||Natural RWS - ML1 Toy||^2')
 ax_iters[3].plot([((m_q[i] - m_q_ml2[i])**2).sum() for i in range(T)], color=colours[2], label=f'||Natural RWS - ML2 Toy||^2')
-ax_iters[3].set_ylabel('MSE of moments')
+ax_iters[3].set_ylabel('MSE of moments against RWS moments')
 ax_iters[3].legend()
 
 
-l_one_iters_diffs = np.array([np.nan] + [l_one_iters[i+1] - l_one_iters[i] for i in range(T-1)])
-l_one_iters_ml1_diffs = np.array([np.nan] + [l_one_iters_ml1[i+1] - l_one_iters_ml1[i] for i in range(T-1)])
-l_one_iters_ml2_diffs = np.array([np.nan] + [l_one_iters_ml2[i+1] - l_one_iters_ml2[i] for i in range(T-1)])
-elbos_ml1_diffs = np.array([np.nan] + [elbos_ml1[i+1] - elbos_ml1[i] for i in range(T-1)])
-elbos_ml2_diffs = np.array([np.nan] + [elbos_ml2[i+1] - elbos_ml2[i] for i in range(T-1)])
+# l_one_iters_diffs = np.array([np.nan] + [l_one_iters[i+1] - l_one_iters[i] for i in range(T-1)])
+# l_one_iters_ml1_diffs = np.array([np.nan] + [l_one_iters_ml1[i+1] - l_one_iters_ml1[i] for i in range(T-1)])
+# l_one_iters_ml2_diffs = np.array([np.nan] + [l_one_iters_ml2[i+1] - l_one_iters_ml2[i] for i in range(T-1)])
+# elbos_ml1_diffs = np.array([np.nan] + [elbos_ml1[i+1] - elbos_ml1[i] for i in range(T-1)])
+# elbos_ml2_diffs = np.array([np.nan] + [elbos_ml2[i+1] - elbos_ml2[i] for i in range(T-1)])
 
 # ax_iters[4].plot(, color=colours[0], label=f'Natural RWS')
+# if not plot_ML2_only:
+#     ax_iters[4].plot(l_one_iters_ml1_diffs - l_one_iters_diffs, color=colours[1], label=f'ML1 Toy')
+#     ax_iters[4].plot(elbos_ml1_diffs - l_one_iters_diffs, color=colours[3], label=f'ML1', linestyle=':')
+# ax_iters[4].plot(l_one_iters_ml2_diffs - l_one_iters_diffs, color=colours[2], label=f'ML2 Toy')
+# ax_iters[4].plot(elbos_ml2_diffs - l_one_iters_diffs, color=colours[4], label=f'ML2', linestyle=':')
+# ax_iters[4].set_ylabel('Difference in ELBO step-sizes against natural_rws')
+# ax_iters[4].legend()
+
+
+ax_iters[4].plot([((prior_params - m_q[i])**2).sum() for i in range(T)], color=colours[0], label=f'Natural RWS')
 if not plot_ML2_only:
-    ax_iters[4].plot(l_one_iters_ml1_diffs - l_one_iters_diffs, color=colours[1], label=f'ML1 Toy')
-    ax_iters[4].plot(elbos_ml1_diffs - l_one_iters_diffs, color=colours[3], label=f'ML1', linestyle=':')
-ax_iters[4].plot(l_one_iters_ml2_diffs - l_one_iters_diffs, color=colours[2], label=f'ML2 Toy')
-ax_iters[4].plot(elbos_ml2_diffs - l_one_iters_diffs, color=colours[4], label=f'ML2', linestyle=':')
-ax_iters[4].set_ylabel('Difference in ELBO step-sizes against natural_rws')
+    ax_iters[4].plot([((prior_params - m_q_ml1[i])**2).sum() for i in range(T)], color=colours[1], label=f'ML1 Toy')
+ax_iters[4].plot([((prior_params - m_q_ml2[i])**2).sum() for i in range(T)], color=colours[2], label=f'ML2 Toy')
+ax_iters[4].set_ylabel('MSE of moments against prior moments')
 ax_iters[4].legend()
 
 ax_iters[-1].set_xlabel('Iteration')
 
 fig_iters.suptitle(f'K={K}, Number of latents={dim_latent}')
 fig_iters.tight_layout()
-fig_iters.savefig(f'figures/ml_diagnostics/N{N}_K{K}_T{T}{"noML1" if plot_ML2_only else ""}.png')
+if backup_plots:
+    fig_iters.savefig(f'figures/ml_diagnostics/N{N}_K{K}_T{T}{"noML1" if plot_ML2_only else ""}.png')
 fig_iters.savefig(f'figures/ml_diagnostic.png')
 plt.close()
 
@@ -188,6 +211,8 @@ diff_ml1_ml1toy = 0
 diff_ml2_ml2toy = 0
 diff_ml1_natural = 0
 diff_ml2_natural = 0
+diff_ml1toy_natural = 0
+diff_ml2toy_natural = 0
 for i in range(T):
     if np.abs(l_one_iters_ml1[i] - elbos_ml1[i]) > 0.001:
         diff_ml1_ml1toy += 1
@@ -197,11 +222,17 @@ for i in range(T):
         diff_ml1_natural += 1
     if np.abs(l_one_iters[i] - elbos_ml2[i]) > 0.001:
         diff_ml2_natural += 1
+    if np.abs(l_one_iters[i] - l_one_iters_ml1[i]) > 0.001:
+        diff_ml1toy_natural += 1
+    if np.abs(l_one_iters[i] - l_one_iters_ml2[i]) > 0.001:
+        diff_ml2toy_natural += 1
 
 print(f'Number of iterations where ML1 and ML1 Toy differ: {diff_ml1_ml1toy}')
 print(f'Number of iterations where ML2 and ML2 Toy differ: {diff_ml2_ml2toy}')
 print(f'Number of iterations where ML1 and Natural RWS differ: {diff_ml1_natural}')
 print(f'Number of iterations where ML2 and Natural RWS differ: {diff_ml2_natural}')
+print(f'Number of iterations where ML1 Toy and Natural RWS differ: {diff_ml1toy_natural}')
+print(f'Number of iterations where ML2 Toy and Natural RWS differ: {diff_ml2toy_natural}')
 
 
 # open the file m_mismatch_count.txt and plot the numbers in each line of the file
@@ -220,7 +251,8 @@ ax.set_xlabel('Iteration')
 ax.set_ylabel('Count')
 ax.set_title(f'Number of mismatched m_new entries\n(between RWS and ML2)\nN={N}, K={K}')
 
-# fig.savefig(f"figures/m_mismatch_countN{N}_K{K}_T{T}.png")
+if backup_plots:
+    fig.savefig(f"figures/ml_diagnostics/m_mismatch_count/N{N}_K{K}_T{T}.png")
 fig.savefig(f"figures/m_mismatch_count.png")
 plt.close()
 
@@ -228,10 +260,10 @@ plt.close()
 #     print(f"{i}\n{m_q[i]}\n{m_q_ml2[i]}\n{m_q_ml2[i]}\n")
 #     input()
 
-# get moment updates (before lr multiplication) from each iteration
-updates_rws = [init] + [(m_q[i+1] - m_q[i]*(1-lr))/lr for i in range(T-1)]
-updates_ml1 = [init] + [(m_q_ml1[i+1] - m_q_ml1[i]*(1-lr))/lr for i in range(T-1)]
-updates_ml2 = [init] + [(m_q_ml2[i+1] - m_q_ml2[i]*(1-lr))/lr for i in range(T-1)]
+# # get moment updates (before lr multiplication) from each iteration
+# updates_rws = [init] + [(m_q[i+1] - m_q[i]*(1-lr))/lr for i in range(T-1)]
+# updates_ml1 = [init] + [(m_q_ml1[i+1] - m_q_ml1[i]*(1-lr))/lr for i in range(T-1)]
+# updates_ml2 = [init] + [(m_q_ml2[i+1] - m_q_ml2[i]*(1-lr))/lr for i in range(T-1)]
 
 # next_command = input("Press enter to continue, b for breakpoint, or q to quit: ")
 # if next_command != 'q':