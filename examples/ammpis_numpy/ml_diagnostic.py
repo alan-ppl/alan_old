5fc4c4964ead86c3034eeb485175eeb1c97e702c
sambowyer <sambowyer2@gmail.com>
2023-09-06 17:44:09 +0100

    Trying experiment where ML2 (alan) treats each latent separately (and replaced m_mismatch_count.png by 'confusion matrix' subplot in main ml_diagnostics.png)

--- examples/ammpis_numpy/ml_diagnostic.py
+++ examples/ammpis_numpy/ml_diagnostic.py
@@ -17,6 +17,7 @@ parser.add_argument('-T', type=int, default=100)
 parser.add_argument('-p', '--plot_ML2_only', default=False, action='store_true')
 parser.add_argument('-b', '--backup_plots',  default=False, action='store_true')
 parser.add_argument('-s', '--seed', type=int, default=0)
+parser.add_argument('-m', '--mismatch_count', default=False, action='store_true')
 args = parser.parse_args()
 
 N = args.N
@@ -25,9 +26,11 @@ T = args.T
 plot_ML2_only = args.plot_ML2_only
 backup_plots = args.backup_plots
 seed = args.seed
+mismatch_count = args.mismatch_count
 
-with open('m_mismatch_count.txt', 'w') as f:
-    pass
+if mismatch_count:
+    with open('m_mismatch_count.txt', 'w') as f:
+        pass
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -61,6 +64,8 @@ def P(tr):
     # # use below lines instead of the above for plated model
     # tr('mu', alan.Normal(0, 1), plates="plate1")
     # tr('obs', alan.Normal(tr['mu'], 1))
+
+    # print(tr['mu'])
 
 class Q_ml1(alan.AlanModule):
     def __init__(self):
@@ -128,9 +133,48 @@ for i in range(T):
 
 print("ML1 done.\n")
 
+# nasty ml2 hack (for N=3)
+#   by treating each latent variable separately, I think the issues relating to `sum_non_dim`
+#   in `extra_log_factor` are dealt with in a way that _might_ mean ML2 behaves like ML2_toy 
+#   the problem is:
+#       1. by treating each latent variable separately, we mess up the random seeding (I'm pretty sure)
+#       2. this is suboptimal specification for an alan model
+#               (although hopefully if we end up changing alan to fix the `sum_non_dim` stuff, 
+#                we won't have to do this weird model specification anymore)
+#
+#   although the elbos of ML2 and ML2_toy (= rws) are still different, the performances _seem_ comparable
+def P_separate(tr):
+    '''
+    Bayesian Gaussian Model
+    '''
+    # breakpoint()
+    tr('mu1', alan.Normal(prior_mean[0], prior_scale[0]))
+    tr('mu2', alan.Normal(prior_mean[1], prior_scale[1]))
+    tr('mu3', alan.Normal(prior_mean[2], prior_scale[2]))
+
+    mu = t.stack([tr['mu1'], tr['mu2'], tr['mu3']])
+    # print(mu.squeeze(1))
+
+    tr('obs', alan.Normal(mu.squeeze(1), lik_scale))
+
+class Q_ml2_separate(alan.AlanModule):
+    def __init__(self):
+        super().__init__()
+        self.mu1 = alan.ML2Normal(sample_shape=(1,))
+        self.mu2 = alan.ML2Normal(sample_shape=(1,))
+        self.mu3 = alan.ML2Normal(sample_shape=(1,))
+
+    def forward(self, tr):
+        tr('mu1', self.mu1())
+        tr('mu2', self.mu2())
+        tr('mu3', self.mu3())
+
 seed_torch(seed)
-q = Q_ml2()
-m2 = alan.Model(P, q).condition(data=data)
+# q = Q_ml2()
+# m2 = alan.Model(P, q).condition(data=data)
+
+q = Q_ml2_separate()
+m2 = alan.Model(P_separate, q).condition(data=data)
 
 elbos_ml2 = []
 for i in range(T):
@@ -160,11 +204,13 @@ ax_iters[1].plot([(l_one_iters[i] - elbos_ml2[i]) for i in range(T)], color=colo
 ax_iters[1].set_ylabel('Difference in ELBO')
 ax_iters[1].legend()
 
-if not plot_ML2_only:
-    ax_iters[2].plot([l_one_iters[i] / l_one_iters_ml1[i] for i in range(T)], color=colours[1], label=f'Natural RWS / ML1 Toy')
-    ax_iters[2].plot([l_one_iters[i] / elbos_ml1[i] for i in range(T)], color=colours[3], label=f'Natural RWS / ML1', linestyle=':')
-ax_iters[2].plot([l_one_iters[i] / l_one_iters_ml2[i] for i in range(T)], color=colours[2], label=f'Natural RWS / ML2 Toy')
-ax_iters[2].plot([l_one_iters[i] / elbos_ml2[i] for i in range(T)], color=colours[4], label=f'Natural RWS / ML2', linestyle=':')
+# if not plot_ML2_only:
+#     ax_iters[2].plot([l_one_iters[i] / l_one_iters_ml1[i] for i in range(T)], color=colours[1], label=f'Natural RWS / ML1 Toy')
+#     ax_iters[2].plot([l_one_iters[i] / elbos_ml1[i] for i in range(T)], color=colours[3], label=f'Natural RWS / ML1', linestyle=':')
+# ax_iters[2].plot([l_one_iters[i] / l_one_iters_ml2[i] for i in range(T)], color=colours[2], label=f'Natural RWS / ML2 Toy')
+# ax_iters[2].plot([l_one_iters[i] / elbos_ml2[i] for i in range(T)], color=colours[4], label=f'Natural RWS / ML2', linestyle=':')
+ax_iters[2].plot([l_one_iters_ml1[i] / elbos_ml1[i] for i in range(T)], color=colours[1], label=f'ML1 Toy / ML1')
+ax_iters[2].plot([l_one_iters_ml2[i] / elbos_ml2[i] for i in range(T)], color=colours[2], label=f'ML2 Toy / ML2')
 ax_iters[2].set_ylabel('Ratio of ELBOs')
 ax_iters[2].legend()
 
@@ -191,14 +237,52 @@ ax_iters[3].legend()
 # ax_iters[4].legend()
 
 
-ax_iters[4].plot([((prior_params - m_q[i])**2).sum() for i in range(T)], color=colours[0], label=f'Natural RWS')
-if not plot_ML2_only:
-    ax_iters[4].plot([((prior_params - m_q_ml1[i])**2).sum() for i in range(T)], color=colours[1], label=f'ML1 Toy')
-ax_iters[4].plot([((prior_params - m_q_ml2[i])**2).sum() for i in range(T)], color=colours[2], label=f'ML2 Toy')
-ax_iters[4].set_ylabel('MSE of moments against prior moments')
-ax_iters[4].legend()
+# ax_iters[4].plot([((prior_params - m_q[i])**2).sum() for i in range(T)], color=colours[0], label=f'Natural RWS')
+# if not plot_ML2_only:
+#     ax_iters[4].plot([((prior_params - m_q_ml1[i])**2).sum() for i in range(T)], color=colours[1], label=f'ML1 Toy')
+# ax_iters[4].plot([((prior_params - m_q_ml2[i])**2).sum() for i in range(T)], color=colours[2], label=f'ML2 Toy')
+# ax_iters[4].set_ylabel('MSE of moments against prior moments')
+# ax_iters[4].legend()
 
-ax_iters[-1].set_xlabel('Iteration')
+
+# Convert elbos to numpy arrays
+elbos = [np.array([y.item() if type(y) == t.tensor else y for y in x]) for x in [l_one_iters, l_one_iters_ml1, l_one_iters_ml2, elbos_ml1, elbos_ml2]]
+
+conf_mat = np.ndarray((5,5), dtype=int)
+for i in range(5):
+    for j in range(5):
+        conf_mat[i,j] = sum(np.abs(elbos[i] - elbos[j]) < 0.001)
+# print(conf_mat)
+
+im = ax_iters[4].imshow(conf_mat, cmap='Greens')
+
+# Add labels to the confusion matrix
+ax_iters[4].set_xticks(np.arange(len(conf_mat)))
+ax_iters[4].set_yticks(np.arange(len(conf_mat)))
+ax_iters[4].set_xticklabels(['Natural RWS', 'ML1 Toy', 'ML2 Toy', 'ML1', 'ML2'])
+ax_iters[4].set_yticklabels(['Natural RWS', 'ML1 Toy', 'ML2 Toy', 'ML1', 'ML2'])
+plt.setp(ax_iters[4].get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+# Add text to the confusion matrix
+for i in range(len(conf_mat)):
+    for j in range(len(conf_mat)):
+        text = ax_iters[4].text(j, i, conf_mat[i, j], ha="center", va="center", color="w")
+
+# Add title
+ax_iters[4].set_title("# Iterations where ELBOs are within 0.001 of each other")
+
+# Add colorbar to the confusion matrix
+cbar = ax_iters[4].figure.colorbar(im, ax=ax_iters[4])
+
+print(f'Number of iterations where ML1 and ML1 Toy differ: {T - conf_mat[1,3]}')
+print(f'Number of iterations where ML2 and ML2 Toy differ: {T - conf_mat[2,4]}')
+print(f'Number of iterations where ML1 and Natural RWS differ: {T - conf_mat[0,3]}')
+print(f'Number of iterations where ML2 and Natural RWS differ: {T - conf_mat[0,4]}')
+print(f'Number of iterations where ML1 Toy and Natural RWS differ: {T - conf_mat[0,1]}')
+print(f'Number of iterations where ML2 Toy and Natural RWS differ: {T - conf_mat[0,2]}')
+
+
+ax_iters[-2].set_xlabel('Iteration')
 
 fig_iters.suptitle(f'K={K}, Number of latents={dim_latent}')
 fig_iters.tight_layout()
@@ -207,54 +291,29 @@ if backup_plots:
 fig_iters.savefig(f'figures/ml_diagnostic.png')
 plt.close()
 
-diff_ml1_ml1toy = 0
-diff_ml2_ml2toy = 0
-diff_ml1_natural = 0
-diff_ml2_natural = 0
-diff_ml1toy_natural = 0
-diff_ml2toy_natural = 0
-for i in range(T):
-    if np.abs(l_one_iters_ml1[i] - elbos_ml1[i]) > 0.001:
-        diff_ml1_ml1toy += 1
-    if np.abs(l_one_iters_ml2[i] - elbos_ml2[i]) > 0.001:
-        diff_ml2_ml2toy += 1
-    if np.abs(l_one_iters[i] - elbos_ml1[i]) > 0.001:
-        diff_ml1_natural += 1
-    if np.abs(l_one_iters[i] - elbos_ml2[i]) > 0.001:
-        diff_ml2_natural += 1
-    if np.abs(l_one_iters[i] - l_one_iters_ml1[i]) > 0.001:
-        diff_ml1toy_natural += 1
-    if np.abs(l_one_iters[i] - l_one_iters_ml2[i]) > 0.001:
-        diff_ml2toy_natural += 1
-
-print(f'Number of iterations where ML1 and ML1 Toy differ: {diff_ml1_ml1toy}')
-print(f'Number of iterations where ML2 and ML2 Toy differ: {diff_ml2_ml2toy}')
-print(f'Number of iterations where ML1 and Natural RWS differ: {diff_ml1_natural}')
-print(f'Number of iterations where ML2 and Natural RWS differ: {diff_ml2_natural}')
-print(f'Number of iterations where ML1 Toy and Natural RWS differ: {diff_ml1toy_natural}')
-print(f'Number of iterations where ML2 Toy and Natural RWS differ: {diff_ml2toy_natural}')
 
 
-# open the file m_mismatch_count.txt and plot the numbers in each line of the file
-with open('m_mismatch_count.txt', 'r') as f:
-    lines = f.readlines()
-    numbers = [int(line.strip()) for line in lines]
+if mismatch_count:
+    # open the file m_mismatch_count.txt and plot the numbers in each line of the file
+    with open('m_mismatch_count.txt', 'r') as f:
+        lines = f.readlines()
+        numbers = [int(line.strip()) for line in lines]
 
-# Create a figure and an axes object
-fig, ax = plt.subplots()
+    # Create a figure and an axes object
+    fig, ax = plt.subplots()
 
-# Plot the numbers on the axes object
-ax.plot(numbers)
+    # Plot the numbers on the axes object
+    ax.plot(numbers)
 
-# Set the x and y labels
-ax.set_xlabel('Iteration')
-ax.set_ylabel('Count')
-ax.set_title(f'Number of mismatched m_new entries\n(between RWS and ML2)\nN={N}, K={K}')
+    # Set the x and y labels
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Count')
+    ax.set_title(f'Number of mismatched m_new entries\n(between RWS and ML2)\nN={N}, K={K}')
 
-if backup_plots:
-    fig.savefig(f"figures/ml_diagnostics/m_mismatch_count/N{N}_K{K}_T{T}.png")
-fig.savefig(f"figures/m_mismatch_count.png")
-plt.close()
+    if backup_plots:
+        fig.savefig(f"figures/ml_diagnostics/m_mismatch_count/N{N}_K{K}_T{T}.png")
+    fig.savefig(f"figures/m_mismatch_count.png")
+    plt.close()
 
 # for i in range(T):
 #     print(f"{i}\n{m_q[i]}\n{m_q_ml2[i]}\n{m_q_ml2[i]}\n")
