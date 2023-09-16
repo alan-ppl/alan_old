831ae286bb80d0e2a84c295c45807588b47300c6
sambowyer <sambowyer2@gmail.com>
2023-09-16 10:51:37 +0100

    Changed bus job submission scripts

--- examples/fast_ml_experiments/run_bus_ML.sh
+++ examples/fast_ml_experiments/run_bus_ML.sh
@@ -1,5 +1,5 @@
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 --cmd python runner_ml.py training.ML=1 dataset=bus_breakdown model=bus_breakdown training.num_iters=5000 training.Ks=[10]
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML1 --cmd python runner_ml.py training.ML=1 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=0.001
 
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 --cmd python runner_ml.py training.ML=2 dataset=bus_breakdown model=bus_breakdown training.num_iters=5000 training.Ks=[10]
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML2 --cmd python runner_ml.py training.ML=2 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=0.001
 
 
--- examples/fast_ml_experiments/run_bus_VI.sh
+++ examples/fast_ml_experiments/run_bus_VI.sh
@@ -1,1 +1,1 @@
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[10] training.num_iters=5000
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BVI --cmd python runner.py dataset=bus_breakdown model=bus_breakdown training.lr=1e-2 training.Ks=[1,3,5,10] training.num_iters=1000
