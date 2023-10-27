f4490f6736f89bc8973642521650614675692446
ThomasHeap <thomaseheap@gmail.com>
2023-10-27 20:21:31 +0100

    running scripts

--- examples/fast_ml_experiments/run_bus_ML.sh
+++ examples/fast_ml_experiments/run_bus_ML.sh
@@ -1,5 +1,8 @@
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML1 --cmd python runner_ml.py training.ML=1 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=0.001
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML1 --cmd python runner_ml.py training.ML=1 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=1e-2
 
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML2 --cmd python runner_ml.py training.ML=2 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=0.001
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML2 --cmd python runner_ml.py training.ML=2 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=1e-1
 
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML1 --cmd python runner_ml.py training.ML=1 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=5e-2
+
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n BML2 --cmd python runner_ml.py training.ML=2 dataset=bus_breakdown model=bus_breakdown training.num_iters=1000 training.Ks=[1,3,5,10] training.lr=5e-1
 
