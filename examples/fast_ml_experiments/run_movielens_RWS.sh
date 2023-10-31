2b0bbcca2e586f947863d3315eebba67523ff208
ThomasHeap <thomaseheap@gmail.com>
2023-10-31 12:35:05 +0000

    movielens scripts

--- examples/fast_ml_experiments/run_movielens_RWS.sh
+++ examples/fast_ml_experiments/run_movielens_RWS.sh
@@ -1,7 +1,7 @@
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=1e-2 training.Ks=[1,3,5,10] training.num_iters=2000
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=1e-2 training.Ks=[1,3,5,10] training.num_iters=2000 training.N=20 training.M=450
 
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=1e-1 training.Ks=[1,3,5,10] training.num_iters=2000
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=1e-1 training.Ks=[1,3,5,10] training.num_iters=2000 training.N=20 training.M=450
 
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=5e-2 training.Ks=[1,3,5,10] training.num_iters=2000
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=5e-2 training.Ks=[1,3,5,10] training.num_iters=2000 training.N=20 training.M=450
 
-lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=5e-1 training.Ks=[1,3,5,10] training.num_iters=2000
+lbatch -c 1 -g 1 -m 22 -t 24 -q cnu -a cosc020762 -n Movie_RWS --cmd python runner_rws.py dataset=movielens model=movielens training.lr=5e-1 training.Ks=[1,3,5,10] training.num_iters=2000 training.N=20 training.M=450
